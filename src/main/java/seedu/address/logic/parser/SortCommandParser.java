e56dcd92e2cbc18820f0b8c8d9d4f6f03c572ebb
Charles Goh <charlesgoh@118-Infinite-Revelation.local>
2017-10-01 16:13:09 +0800

    [FIX][SORT][COMMANDPARSER] Fixed missing Parentheses

--- src/main/java/seedu/address/logic/parser/SortCommandParser.java
+++ src/main/java/seedu/address/logic/parser/SortCommandParser.java
@@ -22,7 +22,7 @@ public class SortCommandParser implements Parser<SortCommand> {
     public SortCommand parse(String args) throws ParseException {
         String trimmedArgs = args.trim();
         if (trimmedArgs.isEmpty()) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE);
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
         }
 
         // Converts arg arrays to lower case to account for caps entries
