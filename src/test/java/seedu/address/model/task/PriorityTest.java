7f0b3b7763f6fcf7d05615a9bc12bb2055d2ba4b
Esilocke <klin_zw@hotmail.com>
2017-10-12 12:37:27 +0800

    Model: [TDD] Expand on testing for priorities

--- src/test/java/seedu/address/model/task/PriorityTest.java
+++ src/test/java/seedu/address/model/task/PriorityTest.java
@@ -1,4 +1,23 @@
 package seedu.address.model.task;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
 public class PriorityTest {
+
+    @Test
+    public void isValidPriority() {
+        // invalid names
+
+        assertFalse(Priority.isValidPriority(" ")); // spaces only
+        assertFalse(Priority.isValidPriority("invalid")); // invalid priority
+        assertFalse(Priority.isValidPriority("777")); // numerical priority out of range
+
+        // valid names
+        assertTrue(Priority.isValidPriority("")); // empty string
+        assertTrue(Priority.isValidPriority("low")); // string representation
+        assertTrue(Priority.isValidPriority("0")); // numerical representation
+    }
 }
