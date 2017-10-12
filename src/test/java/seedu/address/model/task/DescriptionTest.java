4bd2d25ef785383336dea41d4c7a4cdd668d3ff7
Esilocke <klin_zw@hotmail.com>
2017-10-12 12:37:05 +0800

    Model: [TDD] Expand on testing for descriptions

--- src/test/java/seedu/address/model/task/DescriptionTest.java
+++ src/test/java/seedu/address/model/task/DescriptionTest.java
@@ -1,4 +1,20 @@
 package seedu.address.model.task;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
 public class DescriptionTest {
+
+    @Test
+    public void isValidDescription() {
+        // invalid descriptions
+        assertFalse(Decription.isValidDecription(" ")); // spaces only
+
+        // valid descriptions
+        assertTrue(Decription.isValidDecription("")); // empty string
+        assertTrue(Decription.isValidDecription("alphanumerical contents")); // alphanumerical contents
+        assertTrue(Decription.isValidDecription("-")); // single character
+    }
 }
