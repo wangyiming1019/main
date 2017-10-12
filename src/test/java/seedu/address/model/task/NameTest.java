e4b8e1a88288626607b56d68a30b342f4b4b709d
Esilocke <klin_zw@hotmail.com>
2017-10-12 12:37:13 +0800

    Model: [TDD] Expand on testing for names

--- src/test/java/seedu/address/model/task/NameTest.java
+++ src/test/java/seedu/address/model/task/NameTest.java
@@ -1,4 +1,21 @@
 package seedu.address.model.task;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
 public class NameTest {
+
+    @Test
+    public void isValidName() {
+        // invalid names
+        assertFalse(Name.isValidName(" ")); // spaces only
+        assertFalse(Name.isValidName("")); // empty string
+
+        // valid names
+        assertTrue(Name.isValidName(".")); // single character
+        assertTrue(Name.isValidName("buy pencil")); // alphanumerical with spaces
+        assertTrue(Name.isValidName("buy pencil!")); // special symbols
+    }
 }
