ad59c6609bcb82dae1bb4cca5be0c611e7d3784a
Jeffrey Goh <jeffreygohkw@gmail.com>
2017-10-13 14:10:54 +0800

    Update SampleDataUtil

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -21,23 +21,23 @@ public class SampleDataUtil {
     public static Person[] getSamplePersons() {
         try {
             return new Person[] {
-                new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
-                    new Address("Blk 30 Geylang Street 29, #06-40"),
+                new Person(new Name("Alex Yeoh", false), new Phone("87438807", false), new Email("alexyeoh@example.com", false),
+                    new Address("Blk 30 Geylang Street 29, #06-40", false),
                     getTagSet("friends")),
-                new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
-                    new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
+                new Person(new Name("Bernice Yu", false), new Phone("99272758", false), new Email("berniceyu@example.com", false),
+                    new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18", false),
                     getTagSet("colleagues", "friends")),
-                new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
-                    new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
+                new Person(new Name("Charlotte Oliveiro", false), new Phone("93210283", false), new Email("charlotte@example.com", false),
+                    new Address("Blk 11 Ang Mo Kio Street 74, #11-04", false),
                     getTagSet("neighbours")),
-                new Person(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
-                    new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
+                new Person(new Name("David Li", false), new Phone("91031282", false), new Email("lidavid@example.com", false),
+                    new Address("Blk 436 Serangoon Gardens Street 26, #16-43", false),
                     getTagSet("family")),
-                new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
-                    new Address("Blk 47 Tampines Street 20, #17-35"),
+                new Person(new Name("Irfan Ibrahim", false), new Phone("92492021", false), new Email("irfan@example.com", false),
+                    new Address("Blk 47 Tampines Street 20, #17-35", false),
                     getTagSet("classmates")),
-                new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
-                    new Address("Blk 45 Aljunied Street 85, #11-31"),
+                new Person(new Name("Roy Balakrishnan", false), new Phone("92624417", false), new Email("royb@example.com", false),
+                    new Address("Blk 45 Aljunied Street 85, #11-31", false),
                     getTagSet("colleagues"))
             };
         } catch (IllegalValueException e) {
