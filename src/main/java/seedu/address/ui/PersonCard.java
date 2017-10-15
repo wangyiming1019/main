dec153b9cc2ac5be183bf31d7cd68a81cd521481
Wang Yiming <31090843+wangyiming1019@users.noreply.github.com>
2017-10-16 01:16:03 +0800

    Update PersonCard.java

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -2,6 +2,7 @@ package seedu.address.ui;
 
 import java.util.HashMap;
 import java.util.Random;
+
 import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
