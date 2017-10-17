3be877cc49c6df32f6e9580fcee6db17684af4e8
Charles Goh <charlesgoh@118-Infinite-Revelation.local>
2017-10-17 22:28:36 +0800

    [UI][PERSONCARD][IMPORTS] Added import dependencies for Random and HashMap

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -1,5 +1,8 @@
 package seedu.address.ui;
 
+import java.util.HashMap;
+import java.util.Random;
+
 import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
