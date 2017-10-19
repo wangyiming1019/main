c39d30d3b093d417516315354c1573a96b7ffd28
Esilocke <klin_zw@hotmail.com>
2017-10-19 23:37:42 +0800

    [JUnit] Added blank tests for EditTagParser

---
+++ src/test/java/seedu/address/logic/parser/EditTagParserTest.java
@@ -0,0 +1,18 @@
+package seedu.address.logic.parser;
+
+import org.junit.Test;
+
+public class EditTagParserTest {
+    @Test
+    public void invalidInputTest() {
+        // too little args
+        // too many args
+        // args are the same
+        // args are invalid
+    }
+    @Test
+    public void validInputTest() {
+        // case changes
+        // two distinct words
+    }
+}
