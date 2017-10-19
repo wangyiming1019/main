f7e225068edb6ae10d3c1069aaf8cad360fa9112
Charles Goh <charlesgoh@118-Infinite-Revelation.local>
2017-10-19 20:16:14 +0800

    Revert "[REMARK][GUI] Added remark placeholder to GUI"
    This reverts commit e948b199cb1c7d194941260ff9ca076704f2da5f.

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -51,8 +51,6 @@ public class PersonCard extends UiPart<Region> {
     @FXML
     private Label email;
     @FXML
-    private Label remark;
-    @FXML
     private FlowPane tags;
 
     public PersonCard(ReadOnlyPerson person, int displayedIndex) {
