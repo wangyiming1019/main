224396d91dec757f3032e50f4a26f580250c802e
Charles Goh <charlesgoh@118-Infinite-Revelation.local>
2017-10-19 15:32:12 +0800

    [MAINWINDOW] Fixed checkstyle error regarding overly long line

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -190,7 +190,8 @@ public class MainWindow extends UiPart<Region> {
         ResultDisplay resultDisplay = new ResultDisplay();
         resultDisplayPlaceholder.getChildren().add(resultDisplay.getRoot());
 
-        StatusBarFooter statusBarFooter = new StatusBarFooter(prefs.getAddressBookFilePath(), logic.getFilteredPersonList().size());
+        String preferredFilePath = this.prefs.getAddressBookFilePath();
+        StatusBarFooter statusBarFooter = new StatusBarFooter(preferredFilePath, logic.getFilteredPersonList().size());
         statusbarPlaceholder.getChildren().add(statusBarFooter.getRoot());
 
         CommandBox commandBox = new CommandBox(logic);
