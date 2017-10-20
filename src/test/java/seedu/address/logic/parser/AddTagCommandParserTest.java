0255ab8491575ec0d62f577ae2962d763758af3d
Wang Yiming <31090843+wangyiming1019@users.noreply.github.com>
2017-10-21 03:32:19 +0800

    Update AddTagCommandParserTest.java

--- src/test/java/seedu/address/logic/parser/AddTagCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddTagCommandParserTest.java
@@ -4,7 +4,6 @@ import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
-import static seedu.address.logic.parser.ParserUtil.MESSAGE_INVALID_INDEX;
 import static seedu.address.model.tag.Tag.MESSAGE_TAG_CONSTRAINTS;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
