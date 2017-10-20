20d055c6a0a3337c0072b27ad31aefaf4d8ded4b
Wang Yiming <31090843+wangyiming1019@users.noreply.github.com>
2017-10-21 03:32:02 +0800

    Update DeleteTagCommandParserTest.java

--- src/test/java/seedu/address/logic/parser/DeleteTagCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeleteTagCommandParserTest.java
@@ -4,7 +4,6 @@ import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
-import static seedu.address.logic.parser.ParserUtil.MESSAGE_INVALID_INDEX;
 import static seedu.address.model.tag.Tag.MESSAGE_TAG_CONSTRAINTS;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
@@ -58,7 +57,8 @@ public class DeleteTagCommandParserTest {
 
     @Test
     public void parseInvalidArgsFailure() throws Exception {
-        assertParseFailure(parser, DeleteTagCommand.COMMAND_WORD, String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteTagCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, DeleteTagCommand.COMMAND_WORD, 
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteTagCommand.MESSAGE_USAGE));
     }
 }
 
