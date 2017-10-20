6cdfbe1bc54e8e42da9f0096015ce40407f0dffb
Wang Yiming <31090843+wangyiming1019@users.noreply.github.com>
2017-10-21 02:44:00 +0800

    Update DeleteCommandParserTest.java

--- src/test/java/seedu/address/logic/commands/DeleteCommandParserTest.java
+++ src/test/java/seedu/address/logic/commands/DeleteCommandParserTest.java
@@ -1,32 +1,65 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.logic.parser.ParserUtil.MESSAGE_INVALID_INDEX;
+import static seedu.address.model.tag.Tag.MESSAGE_TAG_CONSTRAINTS;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+
+import java.util.ArrayList;
 
 import org.junit.Test;
 
-import seedu.address.logic.commands.DeleteCommand;
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.DeleteTagCommand;
+import seedu.address.model.tag.Tag;
 
-/**
- * As we are only doing white-box testing, our test cases do not cover path variations
- * outside of the DeleteCommand code. For example, inputs "1" and "1 abc" take the
- * same path through the DeleteCommand, and therefore we test only one of them.
- * The path variation for those two cases occur inside the ParserUtil, and
- * therefore should be covered by the ParserUtilTest.
- */
-public class DeleteCommandParserTest {
+public class DeleteTagCommandParserTest {
 
-    private DeleteCommandParser parser = new DeleteCommandParser();
+    private DeleteTagCommandParser parser = new DeleteTagCommandParser();
 
     @Test
-    public void parse_validArgs_returnsDeleteCommand() {
-        assertParseSuccess(parser, "1", new DeleteCommand(INDEX_FIRST_PERSON));
+    public void parseValidIndexAndTagSuccess() throws Exception {
+        ArrayList<Index> indexes = new ArrayList<Index>();
+        indexes.add(INDEX_FIRST_PERSON);
+        indexes.add(INDEX_SECOND_PERSON);
+
+        final String tagName = "friends";
+        Tag tagToDelete = new Tag(tagName);
+        Index targetIndex1 = INDEX_FIRST_PERSON;
+        Index targetIndex2 = INDEX_SECOND_PERSON;
+        String userInput = targetIndex1.getOneBased() + " " + targetIndex2.getOneBased() + " " + PREFIX_TAG.toString()
+                + tagName;
+        assertParseSuccess(parser, userInput, new DeleteTagCommand(tagToDelete, indexes));
+
+        final String nonExistentTagName = "hello";
+        Tag nonExistentTag = new Tag(nonExistentTagName);
+        userInput = targetIndex1.getOneBased() + " " + targetIndex2.getOneBased() + " " + PREFIX_TAG.toString()
+                + nonExistentTagName;
+        assertParseSuccess(parser, userInput, new DeleteTagCommand(nonExistentTag, indexes));
     }
 
     @Test
-    public void parse_invalidArgs_throwsParseException() {
-        assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
+    public void parseInvalidTagFailure() throws Exception {
+        ArrayList<Index> indexes = new ArrayList<Index>();
+        indexes.add(INDEX_FIRST_PERSON);
+        indexes.add(INDEX_SECOND_PERSON);
+
+        final String tagName = "friends";
+        Index targetIndex1 = INDEX_FIRST_PERSON;
+        Index targetIndex2 = INDEX_SECOND_PERSON;
+
+        String userInput = targetIndex1.getOneBased() + " " + targetIndex2.getOneBased() + " " + PREFIX_TAG.toString();
+        assertParseFailure(parser, userInput, MESSAGE_TAG_CONSTRAINTS);
+    }
+
+    @Test
+    public void parseInvalidArgsFailure() throws Exception {
+        assertParseFailure(parser, DeleteTagCommand.COMMAND_WORD, String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteTagCommand.MESSAGE_USAGE));
     }
 }
+
+
