1d06399ed779f8e79bbecde27e3456d2d097f555
Wang Yiming <31090843+wangyiming1019@users.noreply.github.com>
2017-10-21 03:21:28 +0800

    Create DeleteTagCommandParserTest.java

---
+++ src/test/java/seedu/address/logic/parser/DeleteTagCommandParserTest.java
@@ -0,0 +1,65 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.logic.parser.ParserUtil.MESSAGE_INVALID_INDEX;
+import static seedu.address.model.tag.Tag.MESSAGE_TAG_CONSTRAINTS;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.DeleteTagCommand;
+import seedu.address.model.tag.Tag;
+
+public class DeleteTagCommandParserTest {
+
+    private DeleteTagCommandParser parser = new DeleteTagCommandParser();
+
+    @Test
+    public void parseValidIndexAndTagSuccess() throws Exception {
+        ArrayList<Index> indexes = new ArrayList<Index>();
+        indexes.add(INDEX_FIRST_PERSON);
+        indexes.add(INDEX_SECOND_PERSON);
+
+        final String tagName = "friends";
+        Tag tagToDelete = new Tag(tagName);
+        Index targetIndex1 = INDEX_FIRST_PERSON;
+        Index targetIndex2 = INDEX_SECOND_PERSON;
+        String userInput = targetIndex1.getOneBased() + " " + targetIndex2.getOneBased() + " " + PREFIX_TAG.toString()
+                + tagName;
+        assertParseSuccess(parser, userInput, new DeleteTagCommand(tagToDelete, indexes));
+
+        final String nonExistentTagName = "hello";
+        Tag nonExistentTag = new Tag(nonExistentTagName);
+        userInput = targetIndex1.getOneBased() + " " + targetIndex2.getOneBased() + " " + PREFIX_TAG.toString()
+                + nonExistentTagName;
+        assertParseSuccess(parser, userInput, new DeleteTagCommand(nonExistentTag, indexes));
+    }
+
+    @Test
+    public void parseInvalidTagFailure() throws Exception {
+        ArrayList<Index> indexes = new ArrayList<Index>();
+        indexes.add(INDEX_FIRST_PERSON);
+        indexes.add(INDEX_SECOND_PERSON);
+
+        final String tagName = "friends";
+        Index targetIndex1 = INDEX_FIRST_PERSON;
+        Index targetIndex2 = INDEX_SECOND_PERSON;
+
+        String userInput = targetIndex1.getOneBased() + " " + targetIndex2.getOneBased() + " " + PREFIX_TAG.toString();
+        assertParseFailure(parser, userInput, MESSAGE_TAG_CONSTRAINTS);
+    }
+
+    @Test
+    public void parseInvalidArgsFailure() throws Exception {
+        assertParseFailure(parser, DeleteTagCommand.COMMAND_WORD, String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteTagCommand.MESSAGE_USAGE));
+    }
+}
+
+
