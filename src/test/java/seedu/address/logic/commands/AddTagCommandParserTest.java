d4951b7a66dd80b399d24321f958816d8438e4cf
Wang Yiming <31090843+wangyiming1019@users.noreply.github.com>
2017-10-21 02:30:50 +0800

    Add test for add/delete tag commands

---
+++ src/test/java/seedu/address/logic/commands/AddTagCommandParserTest.java
@@ -0,0 +1,57 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.logic.parser.ParserUtil.MESSAGE_INVALID_INDEX;
+import static seedu.address.model.tag.Tag.MESSAGE_TAG_CONSTRAINTS;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.AddTagCommand;
+import seedu.address.model.tag.Tag;
+
+public class AddTagCommandParserTest {
+
+    private AddTagCommandParser parser = new AddTagCommandParser();
+
+    @Test
+    public void parseValidIndexAndTagSuccess() throws Exception {
+        ArrayList<Index> indexes = new ArrayList<Index>();
+        indexes.add(INDEX_FIRST_PERSON);
+        indexes.add(INDEX_SECOND_PERSON);
+
+        final String tagName = "friends";
+        Tag tagToAdd = new Tag(tagName);
+        Index targetIndex1 = INDEX_FIRST_PERSON;
+        Index targetIndex2 = INDEX_SECOND_PERSON;
+        String userInput = targetIndex1.getOneBased() + " " + targetIndex2.getOneBased() + " " + PREFIX_TAG.toString()
+                + tagName;
+        assertParseSuccess(parser, userInput, new AddTagCommand(tagToAdd, indexes));
+    }
+
+    @Test
+    public void parseInvalidTagFailure() throws Exception {
+        ArrayList<Index> indexes = new ArrayList<Index>();
+        indexes.add(INDEX_FIRST_PERSON);
+        indexes.add(INDEX_SECOND_PERSON);
+
+        final String tagName = "friends";
+        Index targetIndex1 = INDEX_FIRST_PERSON;
+        Index targetIndex2 = INDEX_SECOND_PERSON;
+
+        String userInput = targetIndex1.getOneBased() + " " + targetIndex2.getOneBased() + " " + PREFIX_TAG.toString();
+        assertParseFailure(parser, userInput, MESSAGE_TAG_CONSTRAINTS);
+    }
+
+    @Test
+    public void parseInvalidArgsFailure() throws Exception {
+        assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddTagCommand.MESSAGE_USAGE));
+    }
+}
---
+++ src/test/java/seedu/address/logic/commands/DeleteTagCommandTest.java
@@ -0,0 +1,162 @@
+package seedu.address.logic.commands;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.showFirstPersonOnly;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.UndoRedoStack;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.tag.Tag;
+
+/**
+ * Contains integration tests (interaction with the Model) and unit tests for {@code RemoveTagCommand}.
+ */
+public class DeleteTagCommandTest {
+
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    @Test
+    public void execute_validIndexAndTagUnfilteredListSuccess() throws Exception {
+        ArrayList<Index> indexes = new ArrayList<Index>();
+        indexes.add(INDEX_FIRST_PERSON);
+        indexes.add(INDEX_SECOND_PERSON);
+        Tag tagToDelete = new Tag("friends");
+        DeleteTagCommand deleteTagCommand = prepareCommand(indexes, tagToDelete);
+
+        String expectedMessage = String.format(DeleteTagCommand.MESSAGE_DELETE_TAG_SUCCESS, tagToDelete);
+
+        ModelManager expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        expectedModel.deleteTag(tagToDelete, indexes);
+
+        assertCommandSuccess(deleteTagCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_validIndexAndTagFilteredListSuccess() throws Exception {
+        showFirstPersonOnly(model);
+        ArrayList<Index> indexes = new ArrayList<Index>();
+        indexes.add(INDEX_FIRST_PERSON);
+        Tag tagToDelete = new Tag("friends");
+        DeleteTagCommand deleteTagCommand = prepareCommand(indexes, tagToDelete);
+
+        String expectedMessage = String.format(DeleteTagCommand.MESSAGE_DELETE_TAG_SUCCESS, tagToDelete);
+
+        Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        showFirstPersonOnly(expectedModel);
+        expectedModel.deleteTag(tagToDelete, indexes);
+
+        assertCommandSuccess(deleteTagCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_invalidIndexUnfilteredListThrowsCommandException() throws Exception {
+        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
+        ArrayList<Index> indexes = new ArrayList<Index>();
+        indexes.add(outOfBoundIndex);
+        Tag tagToDelete = new Tag("friends");
+        DeleteTagCommand deleteTagCommand = prepareCommand(indexes, tagToDelete);
+
+        assertCommandFailure(deleteTagCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+
+    }
+
+    @Test
+    public void execute_invalidIndexFilteredListThrowsCommandException() throws Exception {
+        showFirstPersonOnly(model);
+        ArrayList<Index> indexes = new ArrayList<Index>();
+        Index outOfBoundIndex = INDEX_SECOND_PERSON;
+        indexes.add(outOfBoundIndex);
+        assertTrue(outOfBoundIndex.getZeroBased() < model.getAddressBook().getPersonList().size());
+
+        Tag tagToDelete = new Tag("friends");
+        DeleteTagCommand deleteTagCommand = prepareCommand(indexes, tagToDelete);
+
+        assertCommandFailure(deleteTagCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void execute_invalidTagUnfilteredListThrowsCommandException() throws Exception {
+        ArrayList<Index> indexes = new ArrayList<Index>();
+        indexes.add(INDEX_FIRST_PERSON);
+        indexes.add(INDEX_SECOND_PERSON);
+        Tag tagToDelete = new Tag("someone");
+        DeleteTagCommand deleteTagCommand = prepareCommand(indexes, tagToDelete);
+
+        assertCommandFailure(deleteTagCommand, model, DeleteTagCommand.MESSAGE_NONEXISTENT_TAG);
+    }
+
+    @Test
+    public void execute_invalidTagFilteredListThrowsCommandException() throws Exception {
+        showFirstPersonOnly(model);
+        ArrayList<Index> indexes = new ArrayList<Index>();
+        indexes.add(INDEX_FIRST_PERSON);
+        Tag tagToDelete = new Tag("someone");
+        DeleteTagCommand deleteTagCommand = prepareCommand(indexes, tagToDelete);
+
+        assertCommandFailure(deleteTagCommand, model, DeleteTagCommand.MESSAGE_NONEXISTENT_TAG);
+    }
+
+    @Test
+    public void execute_validTagNotInUnFilteredListThrowsCommandException() throws Exception {
+        ArrayList<Index> indexes = new ArrayList<Index>();
+        indexes.add(INDEX_FIRST_PERSON);
+        Tag tagToDelete = new Tag("teacher");
+        DeleteTagCommand deleteTagCommand = prepareCommand(indexes, tagToDelete);
+
+        assertCommandFailure(deleteTagCommand, model, DeleteTagCommand.MESSAGE_NONEXISTENT_TAG);
+    }
+
+    @Test
+    public void equals() throws Exception {
+        ArrayList<Index> indexes1 = new ArrayList<Index>();
+        ArrayList<Index> indexes2 = new ArrayList<Index>();
+        indexes1.add(INDEX_FIRST_PERSON);
+        indexes1.add(INDEX_SECOND_PERSON);
+        indexes2.add(INDEX_SECOND_PERSON);
+        Tag firstTag = new Tag("friends");
+        Tag secondTag = new Tag("classmates");
+        final DeleteTagCommand standardCommand = new DeleteTagCommand(firstTag, indexes1);
+
+        // same values -> returns true
+        DeleteTagCommand commandWithSameValues = new DeleteTagCommand(firstTag, indexes1);
+        assertTrue(standardCommand.equals(commandWithSameValues));
+
+        // same object -> returns true
+        assertTrue(standardCommand.equals(standardCommand));
+
+        // null -> returns false
+        assertFalse(standardCommand.equals(null));
+
+        // different types -> returns false
+        assertFalse(standardCommand.equals(new ClearCommand()));
+
+        // different target indexes -> returns false
+        assertFalse(standardCommand.equals(new DeleteTagCommand(firstTag, indexes2)));
+
+        // different target tag -> returns false
+        assertFalse(standardCommand.equals(new DeleteTagCommand(secondTag, indexes1)));
+    }
+
+    /**
+     * Returns an {@code RemoveTagCommand} with parameters {@code targetIndexes} and {@code tagToDelete}
+     */
+    private DeleteTagCommand prepareCommand(ArrayList<Index> targetIndexes, Tag tagToDelete) {
+        DeleteTagCommand deleteTagCommand = new DeleteTagCommand(tagToDelete, targetIndexes);
+        deleteTagCommand.setData(model, new CommandHistory(), new UndoRedoStack());
+        return deleteTagCommand;
+    }
+}
