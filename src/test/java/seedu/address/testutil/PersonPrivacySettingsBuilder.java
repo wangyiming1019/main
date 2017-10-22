97ea849e50c04e757757d20e9255428f62eef440
Jeffrey Goh <jeffreygohkw@gmail.com>
2017-10-23 00:36:16 +0800

    Add PersonPrivacySettingsBuilder

---
+++ src/test/java/seedu/address/testutil/PersonPrivacySettingsBuilder.java
@@ -0,0 +1,90 @@
+package seedu.address.testutil;
+
+import seedu.address.logic.commands.ChangePrivacyCommand.PersonPrivacySettings;
+import seedu.address.model.person.ReadOnlyPerson;
+
+/**
+ * A utility class to help with building PersonPrivacySettings objects.
+ */
+public class PersonPrivacySettingsBuilder {
+
+    private PersonPrivacySettings pps;
+    public PersonPrivacySettingsBuilder() {
+        pps = new PersonPrivacySettings();
+    }
+
+    public PersonPrivacySettingsBuilder(PersonPrivacySettings pps) {
+        this.pps = new PersonPrivacySettings(pps);
+    }
+
+    /**
+     * Returns an {@code PersonPrivacySettings} with fields containing {@code person}'s privacy details
+     */
+    public PersonPrivacySettingsBuilder(ReadOnlyPerson person) {
+        pps = new PersonPrivacySettings();
+        pps.setNameIsPrivate(person.getName().isPrivate());
+        pps.setPhoneIsPrivate(person.getPhone().isPrivate());
+        pps.setEmailIsPrivate(person.getEmail().isPrivate());
+        pps.setAddressIsPrivate(person.getAddress().isPrivate());
+    }
+
+    /**
+     * Sets the {@code nameIsPrivate} of the {@code PersonPrivacySettings} that we are building.
+     */
+    public PersonPrivacySettingsBuilder setNamePrivate(String name) {
+        if (name.equals("Optional[true]") || name.equals("true")) {
+            pps.setNameIsPrivate(true);
+        } else if (name.equals("Optional[false]") || name.equals("false")) {
+            pps.setNameIsPrivate(false);
+        } else {
+            throw new IllegalArgumentException("Privacy of name should be true or false");
+        }
+        return this;
+    }
+
+    /**
+     * Sets the {@code phoneIsPrivate} of the {@code PersonPrivacySettings} that we are building.
+     */
+    public PersonPrivacySettingsBuilder setPhonePrivate(String phone) {
+        if (phone.equals("Optional[true]") || phone.equals("true")) {
+            pps.setPhoneIsPrivate(true);
+        } else if (phone.equals("Optional[false]") || phone.equals("false")) {
+            pps.setPhoneIsPrivate(false);
+        } else {
+            throw new IllegalArgumentException("Privacy of phone should be true or false");
+        }
+        return this;
+    }
+
+    /**
+     * Sets the {@code emailIsPrivate} of the {@code PersonPrivacySettings} that we are building.
+     */
+    public PersonPrivacySettingsBuilder setEmailPrivate(String email) {
+        if (email.equals("Optional[true]") || email.equals("true")) {
+            pps.setEmailIsPrivate(true);
+        } else if (email.equals("Optional[false]") || email.equals("false")) {
+            pps.setEmailIsPrivate(false);
+        } else {
+            throw new IllegalArgumentException("Privacy of email should be true or false");
+        }
+        return this;
+    }
+
+    /**
+     * Sets the {@code addressIsPrivate} of the {@code PersonPrivacySettings} that we are building.
+     */
+    public PersonPrivacySettingsBuilder setAddressPrivate(String address) {
+        if (address.equals("Optional[true]") || address.equals("true")) {
+            pps.setAddressIsPrivate(true);
+        } else if (address.equals("Optional[false]") || address.equals("false")) {
+            pps.setAddressIsPrivate(false);
+        } else {
+            throw new IllegalArgumentException("Privacy of address should be true or false");
+        }
+        return this;
+    }
+
+    public PersonPrivacySettings build() {
+        return pps;
+    }
+}
