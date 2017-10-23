5f7fe5b03db9d92bc5ca58d94c8a39f06b8eaf69
Esilocke <klin_zw@hotmail.com>
2017-10-23 21:09:08 +0800

    [Model] Added constructors for Task object

--- src/main/java/seedu/address/model/task/Task.java
+++ src/main/java/seedu/address/model/task/Task.java
@@ -3,5 +3,24 @@ package seedu.address.model.task;
 /**
  * Represents a task object in the address book.
  */
-public class Task {
+public class Task implements ReadOnlyTask {
+
+    private Name name;
+    private Description description;
+    private Deadline deadline;
+    private Priority priority;
+
+    public Task(Name name, Description description, Deadline deadline, Priority priority) {
+        this.name = name;
+        this.description = description;
+        this.deadline = deadline;
+        this.priority = priority;
+    }
+
+    public Task(ReadOnlyTask task) {
+        this.name = task.getName();
+        this.description = task.getDescription();
+        this.deadline = task.getDeadline();
+        this.priority = task.getPriority();
+    }
 }
