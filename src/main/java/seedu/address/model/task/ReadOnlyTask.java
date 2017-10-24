c610006be5f8a108af093be0d8c402c3ed86a498
Esilocke <klin_zw@hotmail.com>
2017-10-24 11:54:26 +0800

    [Model] Added getAsText method to print out tasks easily

--- src/main/java/seedu/address/model/task/ReadOnlyTask.java
+++ src/main/java/seedu/address/model/task/ReadOnlyTask.java
@@ -5,8 +5,20 @@ package seedu.address.model.task;
  */
 public interface ReadOnlyTask {
 
-    Name getName();
+    TaskName getTaskName();
     Description getDescription();
     Deadline getDeadline();
     Priority getPriority();
+
+    default String getAsText() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append(getTaskName())
+                .append(" Description: ")
+                .append(getDescription())
+                .append(" Deadline: ")
+                .append(getDeadline())
+                .append(" Priority: ")
+                .append(getPriority());
+        return builder.toString();
+    }
 }
