18fd4e1492539c5512ca2641ad0fbc31cd8493f5
Jeffrey Goh <jeffreygohkw@gmail.com>
2017-10-24 22:40:36 +0800

    Update ChangePrivacyCommand

--- src/main/java/seedu/address/logic/commands/ChangePrivacyCommand.java
+++ src/main/java/seedu/address/logic/commands/ChangePrivacyCommand.java
@@ -20,6 +20,7 @@ import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
 import seedu.address.model.person.ReadOnlyPerson;
+import seedu.address.model.person.Remark;
 import seedu.address.model.person.exceptions.DuplicatePersonException;
 import seedu.address.model.person.exceptions.PersonNotFoundException;
 import seedu.address.model.tag.Tag;
