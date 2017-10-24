0a2e9d59cf0a53d633d3ace5b645230600362a32
Esilocke <klin_zw@hotmail.com>
2017-10-24 12:21:12 +0800

    [Model] Added basic add/remove operations for UniqueTasksList

--- src/main/java/seedu/address/model/task/UniqueTasksList.java
+++ src/main/java/seedu/address/model/task/UniqueTasksList.java
@@ -1,11 +1,15 @@
 package seedu.address.model.task;
 
+import static java.util.Objects.requireNonNull;
+
 import java.util.Iterator;
 
 import org.fxmisc.easybind.EasyBind;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import seedu.address.model.task.exceptions.DuplicateTaskException;
+import seedu.address.model.task.exceptions.TaskNotFoundException;
 
 /**
  * Provides a list of Tasks that are unique, and are not null.
@@ -17,6 +21,41 @@ public class UniqueTasksList implements Iterable<Task> {
 
     public ObservableList<ReadOnlyTask> asObservableList() {
         return FXCollections.unmodifiableObservableList(mappedList);
+    }
+
+    /**
+     * Returns true if the list contains an equivalent task as the given argument.
+     */
+    public boolean contains(ReadOnlyTask toCheck) {
+        requireNonNull(toCheck);
+        return internalList.contains(toCheck);
+    }
+
+    /**
+     * Adds a task to the list.
+     *
+     * @throws DuplicateTaskException if the task to add is a duplicate of an existing task in the list.
+     */
+    public void add(ReadOnlyTask toAdd) throws DuplicateTaskException {
+        requireNonNull(toAdd);
+        if (contains(toAdd)) {
+            throw new DuplicateTaskException();
+        }
+        internalList.add(new Task(toAdd));
+    }
+
+    /**
+     * Removes the equivalent task from the list.
+     *
+     * @throws TaskNotFoundException if no such task could be found in the list.
+     */
+    public boolean remove(ReadOnlyTask toRemove) throws TaskNotFoundException {
+        requireNonNull(toRemove);
+        final boolean taskFoundAndDeleted = internalList.remove(toRemove);
+        if (!taskFoundAndDeleted) {
+            throw new TaskNotFoundException();
+        }
+        return taskFoundAndDeleted;
     }
 
     @Override
