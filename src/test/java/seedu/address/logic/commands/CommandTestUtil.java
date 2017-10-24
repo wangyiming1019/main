47713acbabd0177ea0fceb9781d863956c1ba634
Wang Yiming <31090843+wangyiming1019@users.noreply.github.com>
2017-10-25 00:53:16 +0800

    Add favouriteFirstPerson() method
    1. Add favouriteFirstPerson() method
    2. Add favourite value for AMY and BOB

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -39,6 +39,7 @@ public class CommandTestUtil {
     public static final String VALID_ADDRESS_BOB = "Block 123, Bobby Street 3";
     public static final String VALID_TAG_HUSBAND = "husband";
     public static final String VALID_TAG_FRIEND = "friend";
+    public static final String VALID_FAVOURITE = "false";
     public static final String VALID_TAG_COLLEAGUE = "colleague";
 
     public static final String NAME_DESC_AMY = " " + PREFIX_NAME + VALID_NAME_AMY;
@@ -71,10 +72,11 @@ public class CommandTestUtil {
     static {
         DESC_AMY = new EditPersonDescriptorBuilder().withName(VALID_NAME_AMY)
                 .withPhone(VALID_PHONE_AMY).withEmail(VALID_EMAIL_AMY).withAddress(VALID_ADDRESS_AMY)
+                .withAddress(VALID_ADDRESS_AMY).withFavourite(VALID_FAVOURITE)
                 .withTags(VALID_TAG_FRIEND).build();
         DESC_BOB = new EditPersonDescriptorBuilder().withName(VALID_NAME_BOB)
                 .withPhone(VALID_PHONE_BOB).withEmail(VALID_EMAIL_BOB).withAddress(VALID_ADDRESS_BOB)
-                .withTags(VALID_TAG_HUSBAND, VALID_TAG_FRIEND).build();
+                .withFavourite(VALID_FAVOURITE).withTags(VALID_TAG_HUSBAND, VALID_TAG_FRIEND).build();
     }
 
     /**
@@ -133,6 +135,18 @@ public class CommandTestUtil {
         ReadOnlyPerson firstPerson = model.getFilteredPersonList().get(0);
         try {
             model.deletePerson(firstPerson);
+        } catch (PersonNotFoundException pnfe) {
+            throw new AssertionError("Person in filtered list must exist in model.", pnfe);
+        }
+    }
+
+    /**
+     * Favourites the first person in {@code model}'s filtered list from {@code model}'s address book.
+     */
+    public static void favouriteFirstPerson(Model model) {
+        ReadOnlyPerson firstPerson = model.getAddressBook().getPersonList().get(0);
+        try {
+            model.favouritePerson(firstPerson);
         } catch (PersonNotFoundException pnfe) {
             throw new AssertionError("Person in filtered list must exist in model.", pnfe);
         }
