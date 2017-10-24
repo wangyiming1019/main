7168bf0db23e6c54bda6706d3fdeac324ec7b525
Esilocke <klin_zw@hotmail.com>
2017-10-24 11:56:08 +0800

    [Model] Added basic methods for UniqueTasksList

--- src/main/java/seedu/address/model/task/UniqueTasksList.java
+++ src/main/java/seedu/address/model/task/UniqueTasksList.java
@@ -1,4 +1,26 @@
 package seedu.address.model.task;
 
-public class UniqueTasksList {
+import java.util.Iterator;
+
+import org.fxmisc.easybind.EasyBind;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+/**
+ * Provides a list of Tasks that are unique, and are not null.
+ */
+public class UniqueTasksList implements Iterable<Task> {
+    private final ObservableList<Task> internalList = FXCollections.observableArrayList();
+    // used by asObservableList()
+    private final ObservableList<ReadOnlyTask> mappedList = EasyBind.map(internalList, (task) -> task);
+
+    public ObservableList<ReadOnlyTask> asObservableList() {
+        return FXCollections.unmodifiableObservableList(mappedList);
+    }
+
+    @Override
+    public Iterator<Task> iterator() {
+        return internalList.iterator();
+    }
 }
