852dfd162b7bdd57d5cca70826ae099dbb000556
Wang Yiming <31090843+wangyiming1019@users.noreply.github.com>
2017-10-25 00:41:13 +0800

    Create UnFavouriteCommandTest.java
    1. Add valid index valid person test 
    2. Add invalid index test

---
+++ src/test/java/seedu/address/logic/commands/UnFavouriteCommandTest.java
@@ -0,0 +1,79 @@
+package seedu.address.logic.commands;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.favouriteFirstPerson;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.Test;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.UndoRedoStack;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.person.ReadOnlyPerson;
+
+public class UnFavouriteCommandTest {
+
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    @Test
+    public void executeValidIndexValidPersonSuccess() throws Exception {
+        favouriteFirstPerson(model);
+        ReadOnlyPerson personToUnfavourite = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        UnfavouriteCommand unfavouriteCommand = prepareCommand(INDEX_FIRST_PERSON);
+
+        String expectedMessage = String.format(UnfavouriteCommand.MESSAGE_UNFAVOURITE_PERSON_SUCCESS,
+                personToUnfavourite);
+        CommandResult commandResult = unfavouriteCommand.execute();
+
+        assertEquals(expectedMessage, commandResult.feedbackToUser);
+    }
+
+    @Test
+    public void executeInvalidIndexThrowsCommandException() throws Exception {
+        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
+        UnfavouriteCommand unfavouriteCommand = prepareCommand(outOfBoundIndex);
+        assertCommandFailure(unfavouriteCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void equals() {
+        UnfavouriteCommand favouriteFirstCommand = new UnfavouriteCommand(INDEX_FIRST_PERSON);
+        UnfavouriteCommand favouriteSecondCommand = new UnfavouriteCommand(INDEX_SECOND_PERSON);
+
+        // same object -> returns true
+        assertTrue(favouriteFirstCommand.equals(favouriteFirstCommand));
+
+        // same values -> returns true
+        UnfavouriteCommand deleteFirstCommandCopy = new UnfavouriteCommand(INDEX_FIRST_PERSON);
+        assertTrue(favouriteFirstCommand.equals(deleteFirstCommandCopy));
+
+        // different types -> returns false
+        assertFalse(favouriteFirstCommand.equals(1));
+
+        // null -> returns false
+        assertFalse(favouriteFirstCommand.equals(null));
+
+        // different person -> returns false
+        assertFalse(favouriteFirstCommand.equals(favouriteSecondCommand));
+    }
+
+    /**
+     * Returns a {@code UnfavouriteCommand} with the parameter {@code index}.
+     */
+    private UnfavouriteCommand prepareCommand(Index index) {
+        UnfavouriteCommand unfavouriteCommand = new UnfavouriteCommand(index);
+        unfavouriteCommand.setData(model, new CommandHistory(), new UndoRedoStack());
+        return unfavouriteCommand;
+    }
+}
