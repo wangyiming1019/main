5e63a91a53bda591b06d1c565c73364f63f85aac
Esilocke <klin_zw@hotmail.com>
2017-10-26 21:23:30 +0800

    [Logic] Added support for persons clearing, tasks clearing, or full clearing of the address book

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -1,7 +1,10 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PERSON;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TASK;
 
+import seedu.address.logic.parser.Prefix;
 import seedu.address.model.AddressBook;
 
 /**
@@ -11,13 +14,55 @@ public class ClearCommand extends UndoableCommand {
 
     public static final String COMMAND_WORD = "clear";
     public static final String COMMAND_ALIAS = "c";
-    public static final String MESSAGE_SUCCESS = "Address book has been cleared!";
+    public static final String MESSAGE_SUCCESS = "%s has been cleared!";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Clears the address book. "
+            + "Parameters: "
+            + "[" + PREFIX_PERSON + "] "
+            + "[" + PREFIX_TASK + "] ";
 
+    private static final String TYPE_PERSONS = "Contact list";
+    private static final String TYPE_TASKS = "Task list";
+    private static final String TYPE_ALL = "Address book";
+
+    private boolean isClearTask;
+    private boolean isClearPerson;
+    private boolean isClearAll;
+    private String typeCleared;
+
+    public ClearCommand() {
+        isClearAll = true;
+        isClearPerson = false;
+        isClearTask = false;
+        typeCleared = TYPE_ALL;
+    }
+    public ClearCommand(Prefix type) {
+        if (type.getPrefix().equals(PREFIX_TASK)) {
+            isClearTask = true;
+            isClearPerson = false;
+            isClearAll = false;
+            typeCleared = TYPE_TASKS;
+        } else if (type.getPrefix().equals(PREFIX_PERSON)) {
+            isClearPerson = true;
+            isClearTask = false;
+            isClearAll = false;
+            typeCleared = TYPE_PERSONS;
+        } else {
+            throw new AssertionError("An invalid type was provided!");
+        }
+    }
 
     @Override
     public CommandResult executeUndoableCommand() {
         requireNonNull(model);
-        model.resetData(new AddressBook());
-        return new CommandResult(MESSAGE_SUCCESS);
+        if (isClearAll) {
+            model.resetData(new AddressBook());
+        } else if (isClearTask) {
+            model.resetTasks();
+        } else if (isClearPerson) {
+            model.resetContacts();
+        } else {
+            assert false : "At least one boolean must be true.";
+        }
+        return new CommandResult(String.format(MESSAGE_SUCCESS, typeCleared));
     }
 }
