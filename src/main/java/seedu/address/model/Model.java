b1d7f0b054d9b0a9dfccf2b6bce596ba07dd1717
Esilocke <klin_zw@hotmail.com>
2017-10-26 14:20:28 +0800

    [Model] Added new methods to handle task editing

--- src/main/java/seedu/address/model/Model.java
+++ src/main/java/seedu/address/model/Model.java
@@ -19,6 +19,8 @@ import seedu.address.model.task.exceptions.TaskNotFoundException;
 public interface Model {
     /** {@code Predicate} that always evaluate to true */
     Predicate<ReadOnlyPerson> PREDICATE_SHOW_ALL_PERSONS = unused -> true;
+    /** {@code Predicate} that always evaluate to true */
+    Predicate<ReadOnlyTask> PREDICATE_SHOW_ALL_TASKS = unused -> true;
 
     /** Clears existing backing model and replaces with the provided new data. */
     void resetData(ReadOnlyAddressBook newData);
@@ -66,6 +68,16 @@ public interface Model {
     /** Deletes the given task */
     void deleteTask(ReadOnlyTask toDelete) throws TaskNotFoundException;
 
+    /**
+     * Replaces the given task {@code target} with {@code editedTask}.
+     *
+     * @throws DuplicateTaskException if updating the task's details causes the task to be equivalent to
+     *      another existing task in the list.
+     * @throws TaskNotFoundException if {@code target} could not be found in the list.
+     */
+    void updateTask(ReadOnlyTask target, ReadOnlyTask editedTask)
+            throws DuplicateTaskException, TaskNotFoundException;
+
     /** Returns an unmodifiable view of the filtered person list */
     ObservableList<ReadOnlyPerson> getFilteredPersonList();
 
@@ -77,6 +89,12 @@ public interface Model {
      * @throws NullPointerException if {@code predicate} is null.
      */
     void updateFilteredPersonList(Predicate<ReadOnlyPerson> predicate);
+
+    /**
+     * Updates the filter of the filtered task list to filter by the given {@code predicate}.
+     * @throws NullPointerException if {@code predicate} is null.
+     */
+    void updateFilteredTaskList(Predicate<ReadOnlyTask> predicate);
 
     /**
      * Sorts all persons in person list by field in ascending (asc) or descending (desc) order
