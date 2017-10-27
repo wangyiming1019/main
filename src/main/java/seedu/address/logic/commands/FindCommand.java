d77d4554de1eaf91fa3028be5b0bb5d5c2510cd9
Esilocke <klin_zw@hotmail.com>
2017-10-28 00:34:00 +0800

    [Logic] Updated Find command to allow for task finding

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -1,9 +1,10 @@
 package seedu.address.logic.commands;
 
 import seedu.address.model.person.NameContainsKeywordsPredicate;
+import seedu.address.model.task.TaskContainsKeywordPredicate;
 
 /**
- * Finds and lists all persons in address book whose name contains any of the argument keywords.
+ * Finds and lists all persons or tasks in address book whose name contains any of the argument keywords.
  * Keyword matching is case sensitive.
  */
 public class FindCommand extends Command {
@@ -15,23 +16,52 @@ public class FindCommand extends Command {
             + "the specified keywords (case-sensitive) and displays them as a list with index numbers.\n"
             + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
             + "Example: " + COMMAND_WORD + " alice bob charlie";
+    public static final String MESSAGE_TASK_USAGE = COMMAND_WORD + ": Finds all tasks whose names or descriptions "
+            + "contain any of the specified keywords (case-sensitive) and displays them as a list with index numbers.\n"
+            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
+            + "Example: " + COMMAND_WORD + " task make";
 
-    private final NameContainsKeywordsPredicate predicate;
+    private final NameContainsKeywordsPredicate personPredicate;
+    private final TaskContainsKeywordPredicate taskPredicate;
+    private boolean isTask;
 
     public FindCommand(NameContainsKeywordsPredicate predicate) {
-        this.predicate = predicate;
+        this.personPredicate = predicate;
+        this.taskPredicate = null;
+        this.isTask = false;
+    }
+
+    public FindCommand(TaskContainsKeywordPredicate predicate) {
+        this.personPredicate = null;
+        this.taskPredicate = predicate;
+        this.isTask = true;
     }
 
     @Override
     public CommandResult execute() {
-        model.updateFilteredPersonList(predicate);
-        return new CommandResult(getMessageForPersonListShownSummary(model.getFilteredPersonList().size()));
+        if (isTask) {
+            assert(taskPredicate != null);
+            model.updateFilteredTaskList(taskPredicate);
+            return new CommandResult(getMessageForTaskListShownSummary(model.getFilteredTaskList().size()));
+        } else {
+            assert(personPredicate != null);
+            model.updateFilteredPersonList(personPredicate);
+            return new CommandResult(getMessageForPersonListShownSummary(model.getFilteredPersonList().size()));
+        }
     }
 
     @Override
     public boolean equals(Object other) {
-        return other == this // short circuit if same object
-                || (other instanceof FindCommand // instanceof handles nulls
-                && this.predicate.equals(((FindCommand) other).predicate)); // state check
+        if (other == this) {
+            return true;
+        } else if (!(other instanceof FindCommand)) {
+            return false;
+        } else if (this.isTask != ((FindCommand) other).isTask) {
+            return false;
+        } else if (this.isTask) {
+            return this.taskPredicate.equals(((FindCommand) other).taskPredicate);
+        } else {
+            return this.personPredicate.equals(((FindCommand) other).personPredicate);
+        }
     }
 }
