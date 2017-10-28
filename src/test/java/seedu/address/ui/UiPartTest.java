2514136fa6213da688f2a518956a37487b8e9f35
Charles Goh <charlesgoh@118-Infinite-Revelation.local>
2017-10-28 22:47:21 +0800

    [UIPARTTEST] Uncommented tests that used to fail

--- src/test/java/seedu/address/ui/UiPartTest.java
+++ src/test/java/seedu/address/ui/UiPartTest.java
@@ -40,18 +40,18 @@ public class UiPartTest {
     }
 
 
-    //    @Test
-    //    public void constructor_invalidFileUrl_throwsAssertionError() {
-    //        URL invalidFileUrl = getTestFileUrl(INVALID_FILE_PATH);
-    //        thrown.expect(AssertionError.class);
-    //        new TestUiPart<Object>(invalidFileUrl);
-    //    }
-    //
-    //    @Test
-    //    public void constructor_validFileUrl_loadsFile() {
-    //        URL validFileUrl = getTestFileUrl(VALID_FILE_PATH);
-    //        assertEquals(VALID_FILE_ROOT, new TestUiPart<TestFxmlObject>(validFileUrl).getRoot());
-    //    }
+        @Test
+        public void constructor_invalidFileUrl_throwsAssertionError() {
+            URL invalidFileUrl = getTestFileUrl(INVALID_FILE_PATH);
+            thrown.expect(AssertionError.class);
+            new TestUiPart<Object>(invalidFileUrl);
+        }
+
+        @Test
+        public void constructor_validFileUrl_loadsFile() {
+            URL validFileUrl = getTestFileUrl(VALID_FILE_PATH);
+            assertEquals(VALID_FILE_ROOT, new TestUiPart<TestFxmlObject>(validFileUrl).getRoot());
+        }
 
     @Test
     public void constructor_nullFileName_throwsNullPointerException() {
@@ -65,11 +65,11 @@ public class UiPartTest {
         new TestUiPart<Object>(MISSING_FILE_PATH);
     }
 
-    //    @Test
-    //    public void constructor_invalidFileName_throwsAssertionError() {
-    //        thrown.expect(AssertionError.class);
-    //        new TestUiPart<Object>(INVALID_FILE_PATH);
-    //    }
+        @Test
+        public void constructor_invalidFileName_throwsAssertionError() {
+            thrown.expect(AssertionError.class);
+            new TestUiPart<Object>(INVALID_FILE_PATH);
+        }
 
     private URL getTestFileUrl(String testFilePath) {
         String testFilePathInView = "/view/" + testFilePath;
