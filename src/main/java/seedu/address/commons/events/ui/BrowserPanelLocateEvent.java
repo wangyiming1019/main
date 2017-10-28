3235df568742f0048df50e96f2e869d4342bd545
Jeffrey Goh <jeffreygohkw@gmail.com>
2017-10-28 09:43:32 +0800

    Add BrowserPanelLocateEvent

---
+++ src/main/java/seedu/address/commons/events/ui/BrowserPanelLocateEvent.java
@@ -0,0 +1,28 @@
+package seedu.address.commons.events.ui;
+
+import seedu.address.commons.events.BaseEvent;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.ReadOnlyPerson;
+import seedu.address.ui.PersonCard;
+
+/**
+ * Represents a selection change in the Person List Panel
+ */
+public class BrowserPanelLocateEvent extends BaseEvent {
+
+
+    private final ReadOnlyPerson person;
+
+    public BrowserPanelLocateEvent(ReadOnlyPerson person) {
+        this.person = person;
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName();
+    }
+
+    public ReadOnlyPerson getNewSelection() {
+        return person;
+    }
+}
