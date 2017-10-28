9f7ca761bc8dfa25d0ee86380cc2c3731e5e5f29
Jeffrey Goh <jeffreygohkw@gmail.com>
2017-10-28 18:12:17 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/parser/LocateCommandParser.java
+++ src/main/java/seedu/address/logic/parser/LocateCommandParser.java
@@ -5,7 +5,6 @@ import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.LocateCommand;
-import seedu.address.logic.commands.SelectCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
