0e00ab9f852d442bd6b064344cfefa040b82861f
Charles Goh <charlesgoh@118-Infinite-Revelation.local>
2017-10-29 18:11:49 +0800

    [CONTROLLERS] Added password preferences controller skeleton and AuthPreferences object skeleton code

--- src/main/java/seedu/address/ui/SettingsWindow.java
+++ src/main/java/seedu/address/ui/SettingsWindow.java
@@ -1,64 +1,44 @@
 package seedu.address.ui;
-
+import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
 import java.util.logging.Logger;
 
 import javafx.fxml.FXML;
-import javafx.scene.Scene;
-import javafx.scene.layout.Region;
-import javafx.scene.web.WebView;
-import javafx.stage.Stage;
+import javafx.scene.control.TextField;
 import seedu.address.commons.core.LogsCenter;
-import seedu.address.commons.util.FxViewUtil;
 
 /**
  * Controller for a help page
  */
-public class Settings extends UiPart<Region> {
+public class SettingsWindow {
 
-    public static final String USERGUIDE_FILE_PATH = "/docs/UserGuide.html";
-
-    private static final Logger logger = LogsCenter.getLogger(Settings.class);
-    private static final String ICON = "/images/help_icon.png";
-    private static final String FXML = "HelpWindow.fxml";
-    private static final String TITLE = "Help";
+    private static final Logger logger = LogsCenter.getLogger(SettingsWindow.class);
+    private static final String ICON = "/images/address_book_32_alternative.png";
+    private static final String FXML = "view/SettingsWindow.fxml";
+    private static final String TITLE = "Password Settings";
 
     @FXML
-    private WebView browser;
+    private TextField oldPassword;
 
-    private final Stage dialogStage;
+    @FXML
+    private TextField newPassword;
 
-    public Settings() {
-        super(FXML);
-        Scene scene = new Scene(getRoot());
-        //Null passed as the parent stage to make it non-modal.
-        dialogStage = createDialogStage(TITLE, null, scene);
-        dialogStage.setMaximized(true); //TODO: set a more appropriate initial size
-        FxViewUtil.setStageIcon(dialogStage, ICON);
+    @FXML
+    private TextField confirmPassword;
 
-        String userGuideUrl = getClass().getResource(USERGUIDE_FILE_PATH).toString();
-        browser.getEngine().load(userGuideUrl);
+    public void initialize(URL url, ResourceBundle rb) {
+        //TODO
     }
 
-    /**
-     * Shows the help window.
-     * @throws IllegalStateException
-     * <ul>
-     *     <li>
-     *         if this method is called on a thread other than the JavaFX Application Thread.
-     *     </li>
-     *     <li>
-     *         if this method is called during animation or layout processing.
-     *     </li>
-     *     <li>
-     *         if this method is called on the primary stage.
-     *     </li>
-     *     <li>
-     *         if {@code dialogStage} is already showing.
-     *     </li>
-     * </ul>
-     */
-    public void show() {
-        logger.fine("Showing help page about the application.");
-        dialogStage.showAndWait();
+    @FXML
+    private void handleSaveButtonAction(ActionEvent event) {
+
     }
+
+    @FXML
+    private void handleCancelButtonAction(ActionEvent event) {
+
+    }
+
 }
