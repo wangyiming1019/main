91532ea0dcb5d2fc6a10a150f9c0355c2f1701ca
Charles Goh <charlesgoh@118-Infinite-Revelation.local>
2017-10-29 11:01:05 +0800

    [UI][SETTINGS] Added view and controller for password settings

---
+++ src/main/java/seedu/address/ui/SettingsWindow.java
@@ -0,0 +1,64 @@
+package seedu.address.ui;
+
+import java.util.logging.Logger;
+
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.layout.Region;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+import seedu.address.commons.core.LogsCenter;
+import seedu.address.commons.util.FxViewUtil;
+
+/**
+ * Controller for a help page
+ */
+public class Settings extends UiPart<Region> {
+
+    public static final String USERGUIDE_FILE_PATH = "/docs/UserGuide.html";
+
+    private static final Logger logger = LogsCenter.getLogger(Settings.class);
+    private static final String ICON = "/images/help_icon.png";
+    private static final String FXML = "HelpWindow.fxml";
+    private static final String TITLE = "Help";
+
+    @FXML
+    private WebView browser;
+
+    private final Stage dialogStage;
+
+    public Settings() {
+        super(FXML);
+        Scene scene = new Scene(getRoot());
+        //Null passed as the parent stage to make it non-modal.
+        dialogStage = createDialogStage(TITLE, null, scene);
+        dialogStage.setMaximized(true); //TODO: set a more appropriate initial size
+        FxViewUtil.setStageIcon(dialogStage, ICON);
+
+        String userGuideUrl = getClass().getResource(USERGUIDE_FILE_PATH).toString();
+        browser.getEngine().load(userGuideUrl);
+    }
+
+    /**
+     * Shows the help window.
+     * @throws IllegalStateException
+     * <ul>
+     *     <li>
+     *         if this method is called on a thread other than the JavaFX Application Thread.
+     *     </li>
+     *     <li>
+     *         if this method is called during animation or layout processing.
+     *     </li>
+     *     <li>
+     *         if this method is called on the primary stage.
+     *     </li>
+     *     <li>
+     *         if {@code dialogStage} is already showing.
+     *     </li>
+     * </ul>
+     */
+    public void show() {
+        logger.fine("Showing help page about the application.");
+        dialogStage.showAndWait();
+    }
+}
