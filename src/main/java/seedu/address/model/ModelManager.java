ffd4083d0cef838a15f067fbc8c16e4c1807b8d2
Esilocke <klin_zw@hotmail.com>
2017-10-30 16:30:13 +0800

    [Model] Updated isEquals to include Tasks as well

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -252,7 +252,8 @@ public class ModelManager extends ComponentManager implements Model {
         // state check
         ModelManager other = (ModelManager) obj;
         return addressBook.equals(other.addressBook)
-                && filteredPersons.equals(other.filteredPersons);
+                && filteredPersons.equals(other.filteredPersons)
+                && filteredTasks.equals(other.filteredTasks);
     }
 
 }
