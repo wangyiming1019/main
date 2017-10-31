f1d7f979f1df2365c0b84f97f74936dd2b0cd621
Esilocke <klin_zw@hotmail.com>
2017-10-31 10:19:00 +0800

    [Logic] Add new parsers for Assign/Dismiss

---
+++ src/main/java/seedu/address/logic/parser/AssignTaskCommandParser.java
@@ -0,0 +1,6 @@
+package seedu.address.logic.parser;
+
+//@@author Esilocke
+/** Parses input arguments, and creates a new {@code AssignTaskCommand} object**/
+public class AssignTaskCommandParser {
+}
---
+++ src/main/java/seedu/address/logic/parser/DismissTaskCommandParser.java
@@ -0,0 +1,6 @@
+package seedu.address.logic.parser;
+
+//@@author Esilocke
+/** Parses input arguments, and creates a new {@code DismissTaskCommand} object**/
+public class DismissTaskCommandParser {
+}
