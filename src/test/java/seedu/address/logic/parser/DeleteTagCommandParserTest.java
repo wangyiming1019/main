ab77fd508e6260a4125b294b808da4214e8e6cf7
Wang Yiming <31090843+wangyiming1019@users.noreply.github.com>
2017-11-01 03:29:05 +0800

    Add collate

--- src/test/java/seedu/address/logic/parser/DeleteTagCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeleteTagCommandParserTest.java
@@ -1,5 +1,6 @@
 package seedu.address.logic.parser;
 
+//@@author wangyiming1019
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
