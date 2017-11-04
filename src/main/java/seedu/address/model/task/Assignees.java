862b4f5c31bb997423deeffd2fc20aa3695955fa
Esilocke <klin_zw@hotmail.com>
2017-11-04 23:12:44 +0800

    [Model] Changed assignee data structure from Readonlyperson to Index

--- src/main/java/seedu/address/model/task/Assignees.java
+++ src/main/java/seedu/address/model/task/Assignees.java
@@ -1,42 +1,44 @@
 package seedu.address.model.task;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 
+import seedu.address.commons.core.index.Index;
 import seedu.address.model.person.ReadOnlyPerson;
 /**
  * Represents the list of {@code ReadOnlyPerson} assigned to a task.
  * Contains support for some limited modification operations
  */
 public class Assignees {
-    private HashSet<ReadOnlyPerson> assignedList;
+    private ArrayList<Index> assignedList;
 
-    public Assignees(HashSet<ReadOnlyPerson> assignees) {
+    public Assignees(ArrayList<Index> assignees) {
         this.assignedList = assignees;
     }
 
     public Assignees() {
-        this.assignedList = new HashSet<>();
+        this.assignedList = new ArrayList<>();
     }
 
     /** Assigns all {@code ReadOnlyPerson} in the specified list */
-    public void assign(ArrayList<ReadOnlyPerson> personsToAssign) {
-        for (ReadOnlyPerson p : personsToAssign) {
-            assignedList.add(p);
-        }
-    }
-
-    /** Removes all {@code ReadOnlyPerson} from the specified list */
-    public void dismiss(ArrayList<ReadOnlyPerson> personsToDismiss) {
-        for (ReadOnlyPerson p : personsToDismiss) {
-            if (assignedList.contains(p)) {
-                assignedList.remove(p);
+    public void assign(ArrayList<Index> personsToAssign) {
+        for (Index i : personsToAssign) {
+            if (!assignedList.contains(i)) {
+                assignedList.add(i);
             }
         }
     }
 
+    /** Removes all {@code ReadOnlyPerson} from the specified list */
+    public void dismiss(ArrayList<Index> personsToDismiss) {
+        assignedList.removeAll(personsToDismiss);
+    }
+
     public boolean contains(ReadOnlyPerson toFind) {
         return assignedList.contains(toFind);
+    }
+
+    public ArrayList<Index> getList() {
+        return this.assignedList;
     }
 
     @Override
