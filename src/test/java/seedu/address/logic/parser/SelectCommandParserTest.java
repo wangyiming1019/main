936d14f429619cdc4784c53dd0957ac10b96dd2b
Esilocke <klin_zw@hotmail.com>
2017-11-04 09:38:05 +0800

    [JUnit] Added tests for SelectCommandParser

--- src/test/java/seedu/address/logic/parser/SelectCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/SelectCommandParserTest.java
@@ -1,9 +1,11 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TASK;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_TASK;
 
 import org.junit.Test;
 
@@ -20,10 +22,14 @@ public class SelectCommandParserTest {
     @Test
     public void parse_validArgs_returnsSelectCommand() {
         assertParseSuccess(parser, "1", new SelectCommand(INDEX_FIRST_PERSON, false));
+        assertParseSuccess(parser, " " + PREFIX_TASK.getPrefix() + " 1",
+                new SelectCommand(INDEX_FIRST_TASK, true));
     }
 
     @Test
     public void parse_invalidArgs_throwsParseException() {
         assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT, SelectCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, " " + PREFIX_TASK.getPrefix() + " -1",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, SelectCommand.MESSAGE_USAGE));
     }
 }
