a9e1305d7babc30cfb615f37deb23eee7ebd9ce0
Wang Yiming <31090843+wangyiming1019@users.noreply.github.com>
2017-11-05 17:00:07 +0800

    Add CHRIS for taglist test

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -37,18 +37,24 @@ public class CommandTestUtil {
 
     public static final String VALID_NAME_AMY = "Amy Bee";
     public static final String VALID_NAME_BOB = "Bob Choo";
+    public static final String VALID_NAME_CHRIS = "Chris Chan";
     public static final String VALID_PHONE_AMY = "11111111";
     public static final String VALID_PHONE_BOB = "22222222";
+    public static final String VALID_PHONE_CHRIS = "33333333";
     public static final String VALID_EMAIL_AMY = "amy@example.com";
     public static final String VALID_EMAIL_BOB = "bob@example.com";
+    public static final String VALID_EMAIL_CHRIS = "chris@example.com";
     public static final String VALID_ADDRESS_AMY = "Block 312, Amy Street 1";
     public static final String VALID_ADDRESS_BOB = "Block 123, Bobby Street 3";
+    public static final String VALID_ADDRESS_CHRIS = "Block 132, Chris Street 5";
     public static final String VALID_REMARK_AMY = "She sells sea shells on the sea shore";
     public static final String VALID_REMARK_BOB = "He eats, shoots and leaves";
+    public static final String VALID_REMARK_CHRIS = "He likes swimming";
     public static final String VALID_TAG_HUSBAND = "husband";
     public static final String VALID_TAG_FRIEND = "friend";
     public static final String VALID_FAVOURITE = "false";
     public static final String VALID_TAG_COLLEAGUE = "colleague";
+    public static final String VALID_TAG_CHRISTAG = "special";
 
     public static final String VALID_TASK_NAME_PENCIL = "Buy pencil";
     public static final String VALID_TASK_NAME_PAPER = "Buy paper";
@@ -61,14 +67,19 @@ public class CommandTestUtil {
 
     public static final String NAME_DESC_AMY = " " + PREFIX_NAME + VALID_NAME_AMY;
     public static final String NAME_DESC_BOB = " " + PREFIX_NAME + VALID_NAME_BOB;
+    public static final String NAME_DESC_CHRIS = " " + PREFIX_NAME + VALID_NAME_CHRIS;
     public static final String PHONE_DESC_AMY = " " + PREFIX_PHONE + VALID_PHONE_AMY;
     public static final String PHONE_DESC_BOB = " " + PREFIX_PHONE + VALID_PHONE_BOB;
+    public static final String PHONE_DESC_CHRIS = " " + PREFIX_PHONE + VALID_PHONE_CHRIS;
     public static final String EMAIL_DESC_AMY = " " + PREFIX_EMAIL + VALID_EMAIL_AMY;
     public static final String EMAIL_DESC_BOB = " " + PREFIX_EMAIL + VALID_EMAIL_BOB;
+    public static final String EMAIL_DESC_CHRIS = " " + PREFIX_EMAIL + VALID_EMAIL_CHRIS;
     public static final String ADDRESS_DESC_AMY = " " + PREFIX_ADDRESS + VALID_ADDRESS_AMY;
     public static final String ADDRESS_DESC_BOB = " " + PREFIX_ADDRESS + VALID_ADDRESS_BOB;
+    public static final String ADDRESS_DESC_CHRIS = " " + PREFIX_ADDRESS + VALID_ADDRESS_CHRIS;
     public static final String REMARK_DESC_AMY = " " + PREFIX_REMARK + VALID_REMARK_AMY;
     public static final String REMARK_DESC_BOB = " " + PREFIX_REMARK + VALID_REMARK_BOB;
+    public static final String REMARK_DESC_CHRIS = " " + PREFIX_REMARK + VALID_REMARK_CHRIS;
     public static final String TAG_DESC_FRIEND = " " + PREFIX_TAG + VALID_TAG_FRIEND;
     public static final String TAG_DESC_HUSBAND = " " + PREFIX_TAG + VALID_TAG_HUSBAND;
     public static final String NAME_DESC_AMY_PRIVATE = " " + PREFIX_NAME_PRIVATE + VALID_NAME_AMY;
