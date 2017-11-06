a6ec9b501a1ae5328e50726d850495520c4aa2d4
Charles Goh <charles.goh@live.com>
2017-11-06 21:16:44 +0800

    [PRIVACY] Added missing condition in equals method of ChangePrivacyCommand

--- src/main/java/seedu/address/logic/commands/ChangePrivacyCommand.java
+++ src/main/java/seedu/address/logic/commands/ChangePrivacyCommand.java
@@ -285,7 +285,8 @@ public class ChangePrivacyCommand extends UndoableCommand {
                     && getPhoneIsPrivate().equals(c.getPhoneIsPrivate())
                     && getEmailIsPrivate().equals(c.getEmailIsPrivate())
                     && getAddressIsPrivate().equals(c.getAddressIsPrivate())
-                    && getRemarkIsPrivate().equals(c.getRemarkIsPrivate());
+                    && getRemarkIsPrivate().equals(c.getRemarkIsPrivate())
+                    && getAvatarIsPrivate().equals(c.getAvatarIsPrivate());
         }
     }
 }
