ebbf0019af5813fc67681d6dd4b8db422b2c9240
Charles Goh <charles.goh@live.com>
2017-11-08 15:48:03 +0800

    [CPSCOMMAND] Added helper functions to be used by execute()

--- src/main/java/seedu/address/logic/commands/ChangePasswordCommand.java
+++ src/main/java/seedu/address/logic/commands/ChangePasswordCommand.java
@@ -1,14 +1,19 @@
 package seedu.address.logic.commands;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.logging.Logger;
 
 import com.google.common.hash.Hashing;
+
+import seedu.address.commons.core.LogsCenter;
+import seedu.address.commons.exceptions.DataConversionException;
+import seedu.address.model.UserPrefs;
 
 /**
  * Lists all persons in the address book to the user.
  */
 public class ChangePasswordCommand extends Command {
-
     public static final String COMMAND_WORD = "changepassword";
     public static final String COMMAND_ALIAS = "cps";
 
@@ -16,10 +21,14 @@ public class ChangePasswordCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Changes user password. \n"
             + "Format: changepassword [OLD PASSWORD] [NEWPASSWORD] [CONFIRM NEW PASSWORD]\n"
-            + "Example: " + COMMAND_WORD + " password helloworld helloworld\n"
-            + "Example 2: " + COMMAND_ALIAS + " password helloworld hellowrold\n";
+            + "Example: " + COMMAND_WORD + " password mynewpassword111 mynewpassword111\n"
+            + "Example 2: " + COMMAND_ALIAS + " password mynewpassword111 mynewpassword111\n";
 
-    public static final String MESSAGE_INVALID_INPUT = "Invalid Input.\n" + MESSAGE_SUCCESS;
+    public static final String MESSAGE_INVALID_INPUT = "Invalid Input.\n";
+
+    public static final String MESSAGE_PASSWORD_CONFIRMATION_INCORRECT = "Your new passwords do not match\n";
+
+    private final Logger logger = LogsCenter.getLogger(ChangePasswordCommand.class);
 
     private String oldPassword;
     private String newPassword;
@@ -35,8 +44,41 @@ public class ChangePasswordCommand extends Command {
         return Hashing.sha256().hashString(argument, StandardCharsets.UTF_8).toString();
     }
 
+    /**
+     * Forward hashes the user input password and checks if it matches with the encrypted password saved
+     */
+    private boolean isOldPasswordCorrect() {
+        String forwardHashedInputPassword = forwardHash(oldPassword);
+        String forwardHashActualPassword;
+        UserPrefs userPrefs;
+        try {
+            userPrefs = storage.readUserPrefs().get();
+            forwardHashActualPassword = userPrefs.getAddressBookEncryptedPassword();
+            if (forwardHashActualPassword.equals(forwardHashedInputPassword)) {
+                logger.info("Actual password and input password matches");
+                return true;
+            } else {
+                logger.warning("Actual password and input password do not match");
+                return false;
+            }
+        } catch (DataConversionException e) {
+            e.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return false;
+    }
+
+    /**
+     * Takes new input passwords and checks them against one another.
+     */
+    private boolean isNewPasswordInputsSame() {
+        return forwardHash(newPassword).equals(confirmPassword);
+    }
+
     @Override
     public CommandResult execute() {
+
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
