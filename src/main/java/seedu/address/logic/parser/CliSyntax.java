7bf8244175025cc701e532943b3db2cf60790796
Charles Goh <charles.goh@live.com>
2017-11-09 16:44:00 +0800

    [LOCK] Implemented LockCommand

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -37,4 +37,8 @@ public class CliSyntax {
     public static final Prefix PREFIX_NAVIGATE_TO_PERSON = new Prefix("tp/");
     public static final Prefix PREFIX_NAVIGATE_TO_TASK = new Prefix("tt/");
     public static final Prefix PREFIX_NAVIGATE_TO_ADDRESS = new Prefix("ta/");
+
+    public static final Prefix PREFIX_PASSWORD = new Prefix("ps/");
+    public static final Prefix PREFIX_NEWPASSWORD = new Prefix("nps/");
+    public static final Prefix PREFIX_CONFIRM_PASSOWRD = new Prefix("cps/");
 }
