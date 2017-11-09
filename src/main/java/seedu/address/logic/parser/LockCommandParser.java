a4f7048af107a8aed950d70f8b3cdbec375fdf3b
Charles Goh <charles.goh@live.com>
2017-11-09 22:16:58 +0800

    [PARSER] Added lock and unlock command parsers

---
+++ src/main/java/seedu/address/logic/parser/LockCommandParser.java
@@ -0,0 +1,36 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PASSWORD;
+
+import seedu.address.logic.commands.LockCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+//@@author charlesgoh
+/**
+ * Parses input arguments and returns a new LockCommand object
+ */
+public class LockCommandParser implements Parser<LockCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the LockCommand
+     * and returns a LockCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public LockCommand parse(String args) throws ParseException {
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_PASSWORD);
+        String trimmedArgs = args.trim();
+
+        if (!argMultimap.getValue(PREFIX_PASSWORD).isPresent()) {
+            if (trimmedArgs.isEmpty()) {
+                throw new ParseException(
+                        String.format(MESSAGE_INVALID_COMMAND_FORMAT, LockCommand.MESSAGE_USAGE));
+            }
+            String[] nameKeywords = trimmedArgs.split("\\s+");
+            return new LockCommand(nameKeywords[0]);
+        } else {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, LockCommand.MESSAGE_USAGE));
+        }
+    }
+}
---
+++ src/main/java/seedu/address/logic/parser/UnlockCommandParser.java
@@ -0,0 +1,37 @@
+package seedu.address.logic.parser;
+
+//@@author charlesgoh
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PASSWORD;
+
+import seedu.address.logic.commands.UnlockCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Parses input arguments and returns a new LockCommand object
+ */
+public class UnlockCommandParser implements Parser<UnlockCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the LockCommand
+     * and returns a LockCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public UnlockCommand parse(String args) throws ParseException {
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_PASSWORD);
+        String trimmedArgs = args.trim();
+
+        if (!argMultimap.getValue(PREFIX_PASSWORD).isPresent()) {
+            if (trimmedArgs.isEmpty()) {
+                throw new ParseException(
+                        String.format(MESSAGE_INVALID_COMMAND_FORMAT, UnlockCommand.MESSAGE_USAGE));
+            }
+            String[] nameKeywords = trimmedArgs.split("\\s+");
+            return new UnlockCommand(nameKeywords[0]);
+        } else {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, UnlockCommand.MESSAGE_USAGE));
+        }
+    }
+}
