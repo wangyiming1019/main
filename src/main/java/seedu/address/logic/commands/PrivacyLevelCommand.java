00a50205d3ac0dbe07e73d9973d050f982f5229b
Jeffrey Goh <jeffreygohkw@gmail.com>
2017-11-10 01:23:11 +0800

    Update PrivacyLevelCommand

--- src/main/java/seedu/address/logic/commands/PrivacyLevelCommand.java
+++ src/main/java/seedu/address/logic/commands/PrivacyLevelCommand.java
@@ -1,6 +1,10 @@
 package seedu.address.logic.commands;
 //@@author jeffreygohkw
 import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.ReadOnlyPerson;
+import seedu.address.model.person.exceptions.DuplicatePersonException;
+import seedu.address.model.person.exceptions.PersonNotFoundException;
 
 /**
  *
@@ -12,10 +16,11 @@ public class PrivacyLevelCommand extends UndoableCommand{
 
     public static final String CHANGE_PRIVACY_LEVEL_SUCCESS = "Successfully change privacy level to %1$s.";
     public static final String WRONG_PRIVACY_LEVEL_MESSAGE = "Privacy Level can only be 0, 1 or 2";
+    public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book.";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Changes the privacy level of the address book. Level 0 shows all data, level 1 hides private fields and level 2 hides people with at least 1 private field.\n"
-            + "Parameters: LEVEL (must be 0, 1 or 2)\n"
+            + "Parameters: LEVEL (must be 1, 2 or 3)\n"
             + "Example: " + COMMAND_WORD + " 1";
 
     private final int level;
@@ -26,10 +31,22 @@ public class PrivacyLevelCommand extends UndoableCommand{
 
     @Override
     public CommandResult executeUndoableCommand() throws CommandException {
-        if (level < 0 || level > 2) {
+        if (level < 1 || level > 3) {
             throw new CommandException(WRONG_PRIVACY_LEVEL_MESSAGE);
         }
         model.setPrivacyLevel(level);
+        for (int i = 0; i < model.getAddressBook().getPersonList().size(); i++) {
+            ReadOnlyPerson toReplace = model.getAddressBook().getPersonList().get(i);
+            Person newPerson = new Person(model.getAddressBook().getPersonList().get(i));
+            newPerson.setPrivacyLevel(level);
+            try {
+                model.updatePerson(toReplace, newPerson);
+            } catch (DuplicatePersonException e) {
+                throw new CommandException(MESSAGE_DUPLICATE_PERSON);
+            } catch (PersonNotFoundException e) {
+                throw new AssertionError("The target person cannot be missing");
+            }
+        }
         return new CommandResult(String.format(CHANGE_PRIVACY_LEVEL_SUCCESS, Integer.toString(level)));
     }
 
