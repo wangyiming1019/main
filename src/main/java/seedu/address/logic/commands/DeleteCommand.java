b6ece609c48af79ef7b18498b90c4756d5c1a4ba
Esilocke <klin_zw@hotmail.com>
2017-11-09 22:50:14 +0800

    [Logic] Split commands into commands that handle only persons or types to preserve SRP, and made the superclasses abstract

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -1,76 +1,10 @@
 package seedu.address.logic.commands;
 
-import java.util.List;
-
-import seedu.address.commons.core.Messages;
-import seedu.address.commons.core.index.Index;
-import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.model.person.ReadOnlyPerson;
-import seedu.address.model.person.exceptions.PersonNotFoundException;
-import seedu.address.model.task.ReadOnlyTask;
-import seedu.address.model.task.exceptions.TaskNotFoundException;
-
 /**
- * Deletes a person or task identified using its last displayed index from the address book.
+ * Represents an undoable command that deletes an object from the address book.
  */
-public class DeleteCommand extends UndoableCommand {
+public abstract class DeleteCommand extends UndoableCommand {
 
     public static final String COMMAND_WORD = "delete";
     public static final String COMMAND_ALIAS = "d";
-
-    public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Deletes the person or task identified by the index number used in the last listing.\n"
-            + "Parameters: INDEX (must be a positive integer)\n"
-            + "Example: " + COMMAND_WORD + " 1";
-
-    public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Person: %1$s";
-    public static final String MESSAGE_DELETE_TASK_SUCCESS = "Deleted Task: %1$s";
-    public static final int DELETE_TYPE_PERSON = 0;
-    public static final int DELETE_TYPE_TASK = 1;
-
-    private final Index targetIndex;
-    private int type;
-
-    public DeleteCommand(Index targetIndex, int objectType) {
-        this.targetIndex = targetIndex;
-        this.type = objectType;
-    }
-
-
-    @Override
-    public CommandResult executeUndoableCommand() throws CommandException {
-
-        List<ReadOnlyPerson> personsList = model.getFilteredPersonList();
-        List<ReadOnlyTask> tasksList = model.getFilteredTaskList();
-
-        if (targetIndex.getZeroBased() >= personsList.size() && type == DELETE_TYPE_PERSON) {
-            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
-        } else if (targetIndex.getZeroBased() >= tasksList.size() && type == DELETE_TYPE_TASK) {
-            throw new CommandException(Messages.MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
-        }
-
-        try {
-            if (type == DELETE_TYPE_PERSON) {
-                ReadOnlyPerson personToDelete = personsList.get(targetIndex.getZeroBased());
-                model.deletePerson(personToDelete);
-                return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, personToDelete));
-            } else {
-                ReadOnlyTask taskToDelete = tasksList.get(targetIndex.getZeroBased());
-                model.deleteTask(taskToDelete);
-                return new CommandResult(String.format(MESSAGE_DELETE_TASK_SUCCESS, taskToDelete));
-            }
-        } catch (PersonNotFoundException pnfe) {
-            throw new AssertionError("The target person cannot be missing");
-        } catch (TaskNotFoundException tnfe) {
-            throw new AssertionError("The target task cannot be missing");
-        }
-    }
-
-    @Override
-    public boolean equals(Object other) {
-        return other == this // short circuit if same object
-                || (other instanceof DeleteCommand // instanceof handles nulls
-                && this.targetIndex.equals(((DeleteCommand) other).targetIndex)
-                && this.type == ((DeleteCommand) other).type); // state check
-    }
 }
--- src/main/java/seedu/address/logic/commands/SelectCommand.java
+++ src/main/java/seedu/address/logic/commands/SelectCommand.java
@@ -1,68 +1,11 @@
 package seedu.address.logic.commands;
 
-import java.util.List;
-
-import seedu.address.commons.core.EventsCenter;
-import seedu.address.commons.core.Messages;
-import seedu.address.commons.core.index.Index;
-import seedu.address.commons.events.ui.JumpToListRequestEvent;
-import seedu.address.commons.events.ui.JumpToListRequestTaskEvent;
-import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.model.person.ReadOnlyPerson;
-import seedu.address.model.task.ReadOnlyTask;
-
 /**
- * Selects a person identified using it's last displayed index from the address book.
+ * Represents a command that selects an object in the address book.
  */
-public class SelectCommand extends Command {
+public abstract class SelectCommand extends Command {
 
     public static final String COMMAND_WORD = "select";
     public static final String COMMAND_ALIAS = "s";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Selects the person identified by the index number used in the last person listing.\n"
-            + "Parameters: INDEX (must be a positive integer)\n"
-            + "Example: " + COMMAND_WORD + " 1";
-
-    public static final String MESSAGE_SELECT_PERSON_SUCCESS = "Selected Person: %1$s";
-    public static final String MESSAGE_SELECT_TASK_SUCCESS = "Selected Task: %1$s";
-
-    private final Index targetIndex;
-    private boolean isTask;
-
-    public SelectCommand(Index targetIndex, boolean isTask) {
-        this.targetIndex = targetIndex;
-        this.isTask = isTask;
-    }
-
-    @Override
-    public CommandResult execute() throws CommandException {
-
-        List<ReadOnlyPerson> lastShownList = model.getFilteredPersonList();
-        List<ReadOnlyTask> lastShownTaskList = model.getFilteredTaskList();
-
-        if (!isTask && targetIndex.getZeroBased() >= lastShownList.size()) {
-            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
-        } else if (isTask && targetIndex.getZeroBased() >= lastShownTaskList.size()) {
-            throw new CommandException(Messages.MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
-        }
-
-        if (isTask) {
-            EventsCenter.getInstance().post(new JumpToListRequestTaskEvent(targetIndex));
-            return new CommandResult(String.format(MESSAGE_SELECT_TASK_SUCCESS, targetIndex.getOneBased()));
-        } else {
-            EventsCenter.getInstance().post(new JumpToListRequestEvent(targetIndex));
-            return new CommandResult(String.format(MESSAGE_SELECT_PERSON_SUCCESS, targetIndex.getOneBased()));
-        }
-
-
-    }
-
-    @Override
-    public boolean equals(Object other) {
-        return other == this // short circuit if same object
-                || (other instanceof SelectCommand // instanceof handles nulls
-                && this.targetIndex.equals(((SelectCommand) other).targetIndex))
-                && this.isTask == ((SelectCommand) other).isTask; // state check
-    }
 }
