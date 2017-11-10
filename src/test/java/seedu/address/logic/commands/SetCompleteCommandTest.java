82fb09a86047c1cd1382c8ed90e3f87253e085bc
Esilocke <klin_zw@hotmail.com>
2017-11-11 00:00:55 +0800

    [JUnit] Added additional test cases to cover filtered lists for SetComplete and SetIncomplete commands

--- src/test/java/seedu/address/logic/commands/SetCompleteCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SetCompleteCommandTest.java
@@ -4,6 +4,7 @@ import static junit.framework.TestCase.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.showFirstTaskOnly;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_TASK;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_TASK;
 import static seedu.address.testutil.TypicalTasks.getTypicalTasksOnlyAddressBook;
@@ -23,11 +24,11 @@ public class SetCompleteCommandTest {
     private Model model = new ModelManager(getTypicalTasksOnlyAddressBook(), new UserPrefs());
 
     @Test
-    public void execute_validTaskIndex_success() throws Exception {
+    public void execute_unfilteredListValidIndex_success() throws Exception {
         ReadOnlyTask taskToChange = model.getFilteredTaskList().get(INDEX_FIRST_TASK.getZeroBased());
         SetCompleteCommand setCompleteCommand = prepareCommand(INDEX_FIRST_TASK);
 
-        String expectedMessage = String.format(setCompleteCommand.MESSAGE_SUCCESS, taskToChange);
+        String expectedMessage = String.format(SetCompleteCommand.MESSAGE_SUCCESS, taskToChange);
 
         ModelManager expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
         expectedModel.setAsComplete(taskToChange, true);
@@ -36,7 +37,7 @@ public class SetCompleteCommandTest {
     }
 
     @Test
-    public void execute_invalidTaskIndex_failure() throws Exception {
+    public void execute_unfilteredListInvalidIndex_failure() throws Exception {
         Index outOfRangeIndex = Index.fromZeroBased(model.getFilteredTaskList().size());
         SetCompleteCommand setCompleteCommand = prepareCommand(outOfRangeIndex);
 
@@ -52,7 +53,36 @@ public class SetCompleteCommandTest {
         SetCompleteCommand setCompleteCommand = new SetCompleteCommand(INDEX_FIRST_TASK);
         setCompleteCommand.setData(expectedModel, new CommandHistory(), new UndoRedoStack());
 
-        assertCommandFailure(setCompleteCommand, expectedModel, setCompleteCommand.MESSAGE_TASK_ALREADY_COMPLETE);
+        assertCommandFailure(setCompleteCommand, expectedModel, SetCompleteCommand.MESSAGE_TASK_ALREADY_COMPLETE);
+    }
+
+    @Test
+    public void execute_filteredListValidIndex_success() throws Exception {
+        showFirstTaskOnly(model);
+
+        ReadOnlyTask taskToChange = model.getFilteredTaskList().get(INDEX_FIRST_TASK.getZeroBased());
+        SetCompleteCommand setCompleteCommand = prepareCommand(INDEX_FIRST_TASK);
+
+        String expectedMessage = String.format(SetCompleteCommand.MESSAGE_SUCCESS, taskToChange);
+
+        ModelManager expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        expectedModel.setAsComplete(taskToChange, true);
+        showFirstTaskOnly(expectedModel);
+
+        assertCommandSuccess(setCompleteCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_filteredListInvalidIndex_throwsCommandException() {
+        showFirstTaskOnly(model);
+
+        Index outOfBoundIndex = INDEX_SECOND_TASK;
+        // ensures that outOfBoundIndex is still in bounds of address book list
+        assertTrue(outOfBoundIndex.getZeroBased() < model.getAddressBook().getTasksList().size());
+
+        SetCompleteCommand setCompleteCommand = prepareCommand(outOfBoundIndex);
+
+        assertCommandFailure(setCompleteCommand, model, Messages.MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
     }
 
     @Test
--- src/test/java/seedu/address/logic/commands/SetIncompleteCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SetIncompleteCommandTest.java
@@ -4,6 +4,8 @@ import static junit.framework.TestCase.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.showFirstTaskOnly;
+import static seedu.address.logic.commands.CommandTestUtil.showSecondTaskOnly;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_TASK;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_TASK;
 import static seedu.address.testutil.TypicalIndexes.INDEX_THIRD_TASK;
@@ -28,7 +30,7 @@ public class SetIncompleteCommandTest {
         ReadOnlyTask taskToChange = model.getFilteredTaskList().get(INDEX_THIRD_TASK.getZeroBased());
         SetIncompleteCommand setIncompleteCommand = prepareCommand(INDEX_THIRD_TASK);
 
-        String expectedMessage = String.format(setIncompleteCommand.MESSAGE_SUCCESS, taskToChange);
+        String expectedMessage = String.format(SetIncompleteCommand.MESSAGE_SUCCESS, taskToChange);
 
         ModelManager expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
         expectedModel.setAsComplete(taskToChange, false);
@@ -45,12 +47,12 @@ public class SetIncompleteCommandTest {
     }
 
     @Test
-    public void execute_changeCompletedTask_failure() throws Exception {
+    public void execute_changeIncompleteTask_failure() throws Exception {
         ModelManager expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
         SetIncompleteCommand setIncompleteCommand = new SetIncompleteCommand(INDEX_FIRST_TASK);
         setIncompleteCommand.setData(expectedModel, new CommandHistory(), new UndoRedoStack());
 
-        assertCommandFailure(setIncompleteCommand, expectedModel, setIncompleteCommand.MESSAGE_TASK_ALREADY_COMPLETE);
+        assertCommandFailure(setIncompleteCommand, expectedModel, SetIncompleteCommand.MESSAGE_TASK_ALREADY_COMPLETE);
     }
 
     @Test
@@ -73,6 +75,35 @@ public class SetIncompleteCommandTest {
 
         // different task -> returns false
         assertFalse(setFirstCommand.equals(setSecondCommand));
+    }
+
+    @Test
+    public void execute_filteredListValidIndex_success() throws Exception {
+        showSecondTaskOnly(model);
+
+        ReadOnlyTask taskToChange = model.getFilteredTaskList().get(INDEX_FIRST_TASK.getZeroBased());
+        SetIncompleteCommand setIncompleteCommand = prepareCommand(INDEX_FIRST_TASK);
+
+        String expectedMessage = String.format(SetIncompleteCommand.MESSAGE_SUCCESS, taskToChange);
+
+        ModelManager expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        expectedModel.setAsComplete(taskToChange, false);
+        showSecondTaskOnly(expectedModel);
+
+        assertCommandSuccess(setIncompleteCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_filteredListInvalidIndex_throwsCommandException() {
+        showFirstTaskOnly(model);
+
+        Index outOfBoundIndex = INDEX_SECOND_TASK;
+        // ensures that outOfBoundIndex is still in bounds of address book list
+        assertTrue(outOfBoundIndex.getZeroBased() < model.getAddressBook().getTasksList().size());
+
+        SetIncompleteCommand setIncompleteCommand = prepareCommand(outOfBoundIndex);
+
+        assertCommandFailure(setIncompleteCommand, model, Messages.MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
     }
 
     /**
