115e734d2bf0a949e6fc3165dbb38d090cf2e09e
Esilocke <klin_zw@hotmail.com>
2017-11-10 18:33:41 +0800

    [JUnit] Moved ModelStub to a new file since other tests will be using it

---
+++ src/test/java/seedu/address/testutil/ModelStub.java
@@ -0,0 +1,171 @@
+package seedu.address.testutil;
+
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+import java.util.function.Predicate;
+
+import javafx.collections.ObservableList;
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.parser.Prefix;
+import seedu.address.model.Model;
+import seedu.address.model.ReadOnlyAddressBook;
+import seedu.address.model.person.ReadOnlyPerson;
+import seedu.address.model.person.exceptions.DuplicatePersonException;
+import seedu.address.model.person.exceptions.PersonNotFoundException;
+import seedu.address.model.tag.Tag;
+import seedu.address.model.task.ReadOnlyTask;
+import seedu.address.model.task.exceptions.DuplicateTaskException;
+import seedu.address.model.task.exceptions.TaskNotFoundException;
+import seedu.address.ui.MainWindow;
+
+/**
+ * A default model stub that have all of the methods failing.
+ */
+public class ModelStub implements Model {
+    @Override
+    public void addPerson(ReadOnlyPerson person) throws DuplicatePersonException {
+        fail("This method should not be called.");
+    }
+
+    @Override
+    public void addTag(Tag toAdd, ArrayList<Index> targetIndexes)  {
+        fail("This method should not be called.");
+    }
+
+    @Override
+    public void resetData(ReadOnlyAddressBook newData) {
+        fail("This method should not be called.");
+    }
+
+    @Override
+    public void resetPartialData(ReadOnlyAddressBook newData, Prefix prefix) {
+        fail("This method should not be called.");
+    }
+
+    @Override
+    public ReadOnlyAddressBook getAddressBook() {
+        fail("This method should not be called.");
+        return null;
+    }
+
+    //@@author charlesgoh
+    public void increaseFontSize() {
+        fail("This method should not be called.");
+    }
+
+    public void decreaseFontSize() {
+        fail("This method should not be called.");
+    }
+
+    public void resetFontSize() {
+        fail("This method should not be called.");
+    }
+
+    public void setMainWindow(MainWindow mainWindow) {
+        fail("This method should not be called.");
+    }
+    //@@author
+
+    @Override
+    public void deletePerson(ReadOnlyPerson target) throws PersonNotFoundException {
+        fail("This method should not be called.");
+    }
+
+    @Override
+    public void deleteTag(Tag toDelete, ArrayList<Index> targetIndexes) {
+        fail("This method should not be called.");
+    }
+
+    @Override
+    public void updatePerson(ReadOnlyPerson target, ReadOnlyPerson editedPerson)
+            throws DuplicatePersonException {
+        fail("This method should not be called.");
+    }
+
+    @Override
+    public void editTag(Tag toChange, Tag newTag, ArrayList<Index> affectedIndexes) throws PersonNotFoundException,
+            DuplicatePersonException {
+        fail("This method should not be called.");
+    }
+
+    @Override
+    public ObservableList<ReadOnlyPerson> getFilteredPersonList() {
+        fail("This method should not be called.");
+        return null;
+    }
+
+    @Override
+    public void updateFilteredPersonList(Predicate<ReadOnlyPerson> predicate) {
+        fail("This method should not be called.");
+    }
+
+    @Override
+    public void updateFilteredTaskList(Predicate<ReadOnlyTask> predicate) {
+        fail("This method should not be called.");
+    }
+
+    @Override
+    public void sortPersons(String field, String order) {
+        fail("This method should not be called.");
+    }
+
+    @Override
+    public void sortTasks(String field, String order) {
+        fail("This method should not be called.");
+    }
+
+    @Override
+    public void favouritePerson(ReadOnlyPerson target) throws PersonNotFoundException {
+        fail("This method should not be called.");
+    }
+
+    @Override
+    public void addTask(ReadOnlyTask toAdd) throws DuplicateTaskException {
+        fail("This method should not be called.");
+    }
+
+    @Override
+    public void unfavouritePerson(ReadOnlyPerson target) throws PersonNotFoundException {
+        fail("This method should not be called.");
+    }
+
+    @Override
+    public void deleteTask(ReadOnlyTask toDelete) throws TaskNotFoundException {
+        fail("This method should not be called.");
+    }
+
+    @Override
+    public ObservableList<ReadOnlyTask> getFilteredTaskList() {
+        fail("This method should not be called.");
+        return null;
+    }
+
+    @Override
+    public void updateTask(ReadOnlyTask target, ReadOnlyTask editedTask) throws DuplicateTaskException {
+        fail("This method should not be called.");
+    }
+
+    @Override
+    public void assignToTask(ArrayList<ReadOnlyPerson> personsToAssign, ReadOnlyTask taskToAssign)
+            throws TaskNotFoundException, DuplicateTaskException {
+        fail("This method should not be called.");
+    }
+
+    @Override
+    public void dismissFromTask(ArrayList<ReadOnlyPerson> personsToDismiss, ReadOnlyTask taskToDismiss)
+            throws TaskNotFoundException, DuplicateTaskException {
+        fail("This method should not be called.");
+    }
+
+    @Override
+    public void setAsComplete(ReadOnlyTask toSet, boolean isComplete)
+            throws TaskNotFoundException, DuplicateTaskException {
+        fail("This method should not be called.");
+    }
+
+    @Override
+    public void setPrivacyLevel(int level) {
+        fail("This method should not be called.");
+    }
+}
