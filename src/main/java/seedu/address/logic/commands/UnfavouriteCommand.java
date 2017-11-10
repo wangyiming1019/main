184f16773276c784c2fcf4f2767b87b04feaec61
ACER\kyle <wangyiming93@gmail.com>
2017-11-10 22:49:47 +0800

    Fix CustomImportOrder

--- src/main/java/seedu/address/logic/commands/UnfavouriteCommand.java
+++ src/main/java/seedu/address/logic/commands/UnfavouriteCommand.java
@@ -1,8 +1,8 @@
 package seedu.address.logic.commands;
 
+//@@author wangyiming1019
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
-//@@author wangyiming1019
 import java.util.List;
 
 import seedu.address.commons.core.Messages;
