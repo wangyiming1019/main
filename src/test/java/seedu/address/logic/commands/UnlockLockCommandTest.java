599acd761b5df8de37043ddf36520d088e9eb0b2
Charles Goh <charles.goh@live.com>
2017-11-11 20:32:45 +0800

    [UNLOCK][LOCK] Added simple JUnit tests for lock and unlock tests

---
+++ src/test/java/seedu/address/logic/commands/UnlockLockCommandTest.java
@@ -0,0 +1,47 @@
+package seedu.address.logic.commands;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+
+//@@author charlesgoh
+/**
+ * Tests for lock and unlock functionality. Covers implementation across userprefs and model classes
+ */
+public class UnlockLockCommandTest {
+
+    private Model model;
+
+    @Before
+    public void setUp() {
+        model = new ModelManager();
+    }
+
+    @Test
+    public void checkLockFunctionality() throws Exception {
+        // Check initial state is unlocked
+        assertFalse(model.getLockState());
+
+        // Check that lock works
+        model.lockAddressBookFromModel();
+        assertTrue(model.getLockState());
+
+        // Check double lock
+        model.lockAddressBookFromModel();
+        assertTrue(model.getLockState());
+
+        // Check that unlock works
+        model.unlockAddressBookFromModel();
+        assertFalse(model.getLockState());
+
+        // Check double unlock
+        model.unlockAddressBookFromModel();
+        assertFalse(model.getLockState());
+    }
+
+}
