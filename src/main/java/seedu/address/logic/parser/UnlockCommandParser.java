75de7f38002dee6ae7eb6f08aaf42de8cc4ab1ab
Charles Goh <charles.goh@live.com>
2017-11-11 17:20:40 +0800

    [IMPORTS] Removed unused imports

--- src/main/java/seedu/address/logic/parser/UnlockCommandParser.java
+++ src/main/java/seedu/address/logic/parser/UnlockCommandParser.java
@@ -2,9 +2,6 @@ package seedu.address.logic.parser;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PASSWORD;
 
-import java.util.logging.Logger;
-
-import seedu.address.commons.core.LogsCenter;
 import seedu.address.logic.commands.UnlockCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
