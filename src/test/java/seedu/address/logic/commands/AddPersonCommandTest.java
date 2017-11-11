ec61f7546fa0d5b1f329e1df001ad9a4ae1548e7
Esilocke <klin_zw@hotmail.com>
2017-11-11 22:20:53 +0800

    [Appeasement] Removed unused import

--- src/test/java/seedu/address/logic/commands/AddPersonCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddPersonCommandTest.java
@@ -17,7 +17,6 @@ import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ReadOnlyAddressBook;
-import seedu.address.model.UserPrefs;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.ReadOnlyPerson;
 import seedu.address.model.person.exceptions.DuplicatePersonException;
