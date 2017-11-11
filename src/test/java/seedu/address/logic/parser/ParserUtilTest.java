c66e212d6ca2aef9e0031f708f37cfc4650c8f20
Esilocke <klin_zw@hotmail.com>
2017-11-11 17:12:53 +0800

    [JUnit] Added test cases for remaining methods in ParserUtil

--- src/test/java/seedu/address/logic/parser/ParserUtilTest.java
+++ src/test/java/seedu/address/logic/parser/ParserUtilTest.java
@@ -5,7 +5,10 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static seedu.address.logic.parser.ParserUtil.MESSAGE_INVALID_INDEX;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_THIRD_PERSON;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
@@ -16,11 +19,15 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import seedu.address.commons.core.index.Index;
 import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.Address;
+import seedu.address.model.person.Avatar;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Phone;
+import seedu.address.model.person.Remark;
 import seedu.address.model.tag.Tag;
 import seedu.address.model.task.Deadline;
 import seedu.address.model.task.Description;
@@ -29,13 +36,16 @@ import seedu.address.model.task.TaskAddress;
 import seedu.address.model.task.TaskName;
 
 public class ParserUtilTest {
+    private static final boolean PRIVATE_FIELD = true;
+
     private static final String INVALID_NAME = "R@chel";
     private static final String INVALID_PHONE = "+651234";
     private static final String INVALID_ADDRESS = " ";
     private static final String INVALID_EMAIL = "example.com";
+    private static final String INVALID_REMARK = " ";
+    private static final String INVALID_AVATAR = " ";
     private static final String INVALID_TAG = "#friend";
     private static final String INVALID_TASK_NAME = " ";
-    private static final String INVALID_DESCRIPTION = "";
     private static final String INVALID_DEADLINE = "The distant, distant past";
     private static final String INVALID_PRIORITY = "99";
     private static final String INVALID_TASK_ADDRESS = " ";
@@ -44,6 +54,8 @@ public class ParserUtilTest {
     private static final String VALID_PHONE = "123456";
     private static final String VALID_ADDRESS = "123 Main Street #0505";
     private static final String VALID_EMAIL = "rachel@example.com";
+    private static final String VALID_REMARK = "Thinks that P=NP";
+    private static final String VALID_AVATAR = "valid_file.png";
     private static final String VALID_TAG_1 = "friend";
     private static final String VALID_TAG_2 = "neighbour";
     private static final String VALID_TASK_NAME = "Launcher";
@@ -98,8 +110,11 @@ public class ParserUtilTest {
     public void parseName_validValue_returnsName() throws Exception {
         Name expectedName = new Name(VALID_NAME);
         Optional<Name> actualName = ParserUtil.parseName(Optional.of(VALID_NAME));
+        Name expectedPrivateName = new Name(VALID_NAME, PRIVATE_FIELD);
+        Optional<Name> actualPrivateName = ParserUtil.parseName(Optional.of(VALID_NAME));
 
         assertEquals(expectedName, actualName.get());
+        assertEquals(expectedPrivateName, actualPrivateName.get());
     }
 
     @Test
@@ -123,8 +138,11 @@ public class ParserUtilTest {
     public void parsePhone_validValue_returnsPhone() throws Exception {
         Phone expectedPhone = new Phone(VALID_PHONE);
         Optional<Phone> actualPhone = ParserUtil.parsePhone(Optional.of(VALID_PHONE));
+        Phone expectedPrivatePhone = new Phone(VALID_PHONE, PRIVATE_FIELD);
+        Optional<Phone> actualPrivatePhone = ParserUtil.parsePhone(Optional.of(VALID_PHONE));
 
         assertEquals(expectedPhone, actualPhone.get());
+        assertEquals(expectedPrivatePhone, actualPrivatePhone.get());
     }
 
     @Test
@@ -148,8 +166,11 @@ public class ParserUtilTest {
     public void parseAddress_validValue_returnsAddress() throws Exception {
         Address expectedAddress = new Address(VALID_ADDRESS);
         Optional<Address> actualAddress = ParserUtil.parseAddress(Optional.of(VALID_ADDRESS));
+        Address expectedPrivateAddress = new Address(VALID_ADDRESS, PRIVATE_FIELD);
+        Optional<Address> actualPrivateAddress = ParserUtil.parseAddress(Optional.of(VALID_ADDRESS));
 
         assertEquals(expectedAddress, actualAddress.get());
+        assertEquals(expectedPrivateAddress, actualPrivateAddress.get());
     }
 
     @Test
@@ -173,8 +194,64 @@ public class ParserUtilTest {
     public void parseEmail_validValue_returnsEmail() throws Exception {
         Email expectedEmail = new Email(VALID_EMAIL);
         Optional<Email> actualEmail = ParserUtil.parseEmail(Optional.of(VALID_EMAIL));
+        Email expectedPrivateEmail = new Email(VALID_EMAIL, PRIVATE_FIELD);
+        Optional<Email> actualPrivateEmail = ParserUtil.parseEmail(Optional.of(VALID_EMAIL));
 
         assertEquals(expectedEmail, actualEmail.get());
+        assertEquals(expectedPrivateEmail, actualPrivateEmail.get());
+    }
+
+    @Test
+    public void parseRemark_null_throwsNullPointerException() throws Exception {
+        thrown.expect(NullPointerException.class);
+        ParserUtil.parseRemark(null);
+    }
+
+    @Test
+    public void parseRemark_invalidValue_throwsIllegalValueException() throws Exception {
+        thrown.expect(IllegalValueException.class);
+        ParserUtil.parseRemark(Optional.of(INVALID_REMARK));
+    }
+
+    @Test
+    public void parseRemark_optionalEmpty_returnsOptionalEmpty() throws Exception {
+        assertFalse(ParserUtil.parseRemark(Optional.empty()).isPresent());
+    }
+
+    @Test
+    public void parseRemark_validValue_returnsRemark() throws Exception {
+        Remark expectedRemark = new Remark(VALID_REMARK);
+        Optional<Remark> actualRemark = ParserUtil.parseRemark(Optional.of(VALID_REMARK));
+        Remark expectedPrivateRemark = new Remark(VALID_REMARK, PRIVATE_FIELD);
+        Optional<Remark> actualPrivateRemark = ParserUtil.parseRemark(Optional.of(VALID_REMARK));
+
+        assertEquals(expectedRemark, actualRemark.get());
+        assertEquals(expectedPrivateRemark, actualPrivateRemark.get());
+    }
+
+    @Test
+    public void parseAvatar_null_throwsNullPointerException() throws Exception {
+        thrown.expect(NullPointerException.class);
+        ParserUtil.parseAvatar(null);
+    }
+
+    @Test
+    public void parseAvatar_invalidValue_throwsIllegalValueException() throws Exception {
+        thrown.expect(IllegalValueException.class);
+        ParserUtil.parseAvatar(Optional.of(INVALID_AVATAR));
+    }
+
+    @Test
+    public void parseAvatar_optionalEmpty_returnsOptionalEmpty() throws Exception {
+        assertFalse(ParserUtil.parseAvatar(Optional.empty()).isPresent());
+    }
+
+    @Test
+    public void parseAvatar_validValue_returnsAvatar() throws Exception {
+        Avatar expectedAvatar = new Avatar(VALID_AVATAR);
+        Optional<Avatar> actualAvatar = ParserUtil.parseAvatar(Optional.of(VALID_AVATAR));
+
+        assertEquals(expectedAvatar, actualAvatar.get());
     }
 
     @Test
@@ -319,5 +396,26 @@ public class ParserUtilTest {
         Optional<TaskAddress> actualTaskAddress = ParserUtil.parseTaskAddress(Optional.of(VALID_TASK_ADDRESS));
 
         assertEquals(expectedTaskAddress, actualTaskAddress.get());
+    }
+
+    @Test
+    public void parseIndexes_null_throwsNullPointerException() throws Exception {
+        thrown.expect(NullPointerException.class);
+        ParserUtil.parseIndexes(null);
+    }
+
+    @Test
+    public void parseIndexes_invalidValue_throwsParseException() throws Exception {
+        thrown.expect(ParseException.class);
+        ParserUtil.parseIndexes("1 2 a b c");
+    }
+
+    @Test
+    public void parseIndexes_validValue_returnsIndexes() throws Exception {
+        ArrayList<Index> expectedIndexes = new ArrayList<>(Arrays.asList(INDEX_FIRST_PERSON, INDEX_SECOND_PERSON,
+                INDEX_THIRD_PERSON));
+
+        assertEquals(expectedIndexes, ParserUtil.parseIndexes("1 2 3"));
+        assertEquals(expectedIndexes, ParserUtil.parseIndexes("     1   2    3   "));
     }
 }
