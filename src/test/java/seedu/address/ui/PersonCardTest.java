09840244de9feb4e3f1284e6bafde395792f3d43
Wang Yiming <31090843+wangyiming1019@users.noreply.github.com>
2017-11-12 21:47:15 +0800

    Update PersonCardTest.java

--- src/test/java/seedu/address/ui/PersonCardTest.java
+++ src/test/java/seedu/address/ui/PersonCardTest.java
@@ -60,7 +60,6 @@ public class PersonCardTest extends GuiUnitTest {
             personWithTags.setEmail(ALICE.getEmail());
             personWithTags.setPhone(ALICE.getPhone());
             personWithTags.setTags(ALICE.getTags());
-            personWithTags.setFavourite(ALICE.getFavourite());
         });
         assertCardDisplay(personCard, personWithTags, 2);
     }
